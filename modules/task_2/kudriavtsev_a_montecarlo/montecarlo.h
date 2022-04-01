0fc0838f039b4c5b6d7decc392bd963a99f11cd0
MADZEROPIE <ask_ii1@mail.ru>
2022-04-01 19:34:02 +0300

    omp done

---
+++ modules/task_2/kudriavtsev_a_montecarlo/montecarlo.h
@@ -0,0 +1,17 @@
+// Copyright 2022 KUDRIAVTSEV ALEKSANDR
+#ifndef MODULES_TASK_2_KUDRIAVTSEV_A_MONTECARLO_MONTECARLO_H_
+#define MODULES_TASK_2_KUDRIAVTSEV_A_MONTECARLO_MONTECARLO_H_
+
+#include <omp.h>
+
+#include <random>
+#include <vector>
+
+double MonteCarloSeq(double(*func)(const std::vector<double>&),
+    const std::vector<double>& left, const std::vector<double>& right,
+    uint64_t steps, int seed = 0);
+
+double MonteCarloOmp(double(*func)(const std::vector<double>&),
+    const std::vector<double>& left, const std::vector<double>& right,
+    uint64_t steps, int threadnum, int seed = 0);
+#endif  // MODULES_TASK_2_KUDRIAVTSEV_A_MONTECARLO_MONTECARLO_H_
