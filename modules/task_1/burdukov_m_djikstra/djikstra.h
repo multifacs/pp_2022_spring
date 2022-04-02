64df6a7d82fb20dee49d0400464a77dbe3414127
MikhailBurdukov <burdukovmikhail@gmail.com>
2022-04-02 19:02:17 +0300

    Burdukov Lab1

---
+++ modules/task_1/burdukov_m_djikstra/djikstra.h
@@ -0,0 +1,8 @@
+// Copyright 2022 Burdukov Mikhail
+#ifndef MODULES_TASK_1_BURDUKOV_M_DJIKSTRA_DJIKSTRA_H_
+#define MODULES_TASK_1_BURDUKOV_M_DJIKSTRA_DJIKSTRA_H_
+#include <vector>
+
+std::vector<int> dijkstra(const int start,
+                          const std::vector<std::vector<int>>& matrix);
+#endif  // MODULES_TASK_1_BURDUKOV_M_DJIKSTRA_DJIKSTRA_H_
