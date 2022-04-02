5cb61a246613ba222fa0393900e1da186a3b88eb
Alex Ershov <AlexWE36@mail.ru>
2022-04-02 18:58:39 +0300

    sixth test added

--- modules/task_1/ershov_a_deicstra_algorithm/main.cpp
+++ modules/task_1/ershov_a_deicstra_algorithm/main.cpp
@@ -59,11 +59,30 @@ TEST(Sequential_Deicstra, Test_Sequential_Deicstra_With_Static_Data_Last_Top) {
   ASSERT_EQ(check, true);
 }
 
-TEST(Sequential_Deicstra,
-     Test_Sequential_Deicstra_With_Static_Data_Last_Top_Two_Times) {
+TEST(Sequential_Deicstra, Test_Sequential_Deicstra_Top_Two_Times_Static) {
   const std::vector<std::vector<int>> graf = {
       {0, 7, 9, 0, 0, 14},  {7, 0, 10, 15, 0, 0}, {9, 10, 0, 11, 0, 2},
       {0, 15, 11, 0, 6, 0}, {0, 0, 0, 6, 0, 9},   {14, 0, 2, 0, 9, 0}};
+  bool check = true;
+  const size_t count = graf.size();
+
+  const std::vector<int> algorithmResultFirst =
+      getSequentialDeicstra(graf, count - 1);
+
+  const std::vector<int> algorithmResultSecond =
+      getSequentialDeicstra(graf, count - 1);
+
+  for (size_t i = 0; i < count; ++i) {
+    if (algorithmResultFirst[i] != algorithmResultSecond[i]) {
+      check = false;
+    }
+  }
+
+  ASSERT_EQ(check, true);
+}
+
+TEST(Sequential_Deicstra, Test_Sequential_Deicstra_Top_Two_Times_Random) {
+  const std::vector<std::vector<int>> graf = getRandomVector(10);
   bool check = true;
   const size_t count = graf.size();
 
