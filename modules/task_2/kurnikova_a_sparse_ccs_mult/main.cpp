4ad99382f7b80638f3fa46de210c3d5a2fa38074
alxndrvna <alxndrvna@icloud.com>
2022-04-04 23:45:08 +0300

    omp1

---
+++ modules/task_2/kurnikova_a_sparse_ccs_mult/main.cpp
@@ -0,0 +1,54 @@
+// Copyright 2022 Kurnikova Anastasia
+#include <gtest/gtest.h>
+#include <omp.h>
+#include "./sparse_ccs_mult.h"
+
+TEST(sparse_ccs_mult, test1) {
+    ASSERT_NO_THROW(mymat a(2, 2, 2));
+}
+
+TEST(sparse_ccs_mult, test2) {
+    mymat a(1, 1, 0);
+    mymat b(1, 1, 0);
+    EXPECT_EQ(a, b);
+}
+
+TEST(sparse_ccs_mult, test3) {
+    mymat a(5, 1, 2);
+    ASSERT_NO_THROW(t(&a));
+}
+
+TEST(sparse_ccs_mult, test4) {
+    mymat a(1, 1, 0);
+    mymat b(5, 4, 1);
+    bool q = true;
+    if (a == b)
+        q = true;
+    else
+        q = false;
+    EXPECT_EQ(false, q);
+}
+
+TEST(sparse_ccs_mult, test5) {
+    ASSERT_NO_THROW(randmat(100, 100));
+}
+
+TEST(sparse_ccs_mult, test6) {
+    mymat a(2, 2, 0);
+    mymat b(2, 2, 1);
+    mymat ab(2, 2, 0);
+    EXPECT_EQ(ab, result(&a, &b));
+}
+
+TEST(sparse_ccs_mult, test7) {
+    double seq = ex(1000, 1000, 1000, 1);
+    double omp = ex(1000, 1000, 1000, 4);
+    std::cout << "\n         Sequen: " << seq << "\n";
+    std::cout << "         OpenMP: " << omp << "\n" << std::endl;
+    ASSERT_NO_THROW();
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
