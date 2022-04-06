f689e085f268f05c80496274efd3103d18c521f4
Belyaev ilya <jimmywhiteer@gmail.com>
2022-04-06 15:04:13 +0300

    second

--- modules/task_1/belyaev_i_hoar_sort_simple_fusion/hoar_sort_simple_fusion.h
+++ modules/task_1/belyaev_i_hoar_sort_simple_fusion/hoar_sort_simple_fusion.h
@@ -5,8 +5,8 @@
 #include <vector>
 
 std::vector<int> random_gen(int size);
-void hoar_sort(int left, int right, std::vector<int>* arr);
-bool checker(std::vector<int>* vec);
+void hoar_sort(int left, int right, std::vector <int>* arr);
+bool checker(std::vector <int>* vec);
 std::vector<int> merge(const std::vector<int>& a, const std::vector<int>& b);
 
 #endif  // MODULES_TASK_1_BELYAEV_I_HOAR_SORT_SIMPLE_FUSION_HOAR_SORT_SIMPLE_FUSION_H_
