1a2b4c7e294cb8e6d3e7e862b452492bf11e530e
IvanovAA-bin <ivanov-arkadij1@mail.ru>
2022-04-07 15:40:41 +0300

    style fix

--- modules/task_1/ivanov_a_radix_batchers_mergesort/rbms.cpp
+++ modules/task_1/ivanov_a_radix_batchers_mergesort/rbms.cpp
@@ -1,5 +1,5 @@
 // Copyright 2022 Ivanov Arkady
-#include "./rbms.h"
+#include "../../../modules/task_1/ivanov_a_radix_batchers_mergesort/rbms.h"
 
 // Unused is this lab
 int partner(int nodeIndex, int mergeStage, int mergeStageStep) {
@@ -9,7 +9,7 @@ int partner(int nodeIndex, int mergeStage, int mergeStageStep) {
     if (mergeStageStep == 1) {
         return nodeIndex ^ (1 << (mergeStage - 1));
     } else {
-        int scale = 1 << (mergeStage - mergeStageStep); // difference between nodes for stage n stageStep n.s
+        int scale = 1 << (mergeStage - mergeStageStep);  // difference between nodes for stage n stageStep n.s
         int box = 1 << mergeStageStep;
         int sn = nodeIndex / scale - (nodeIndex / scale / box) * box;
         if (sn == 0 || sn == box - 1)
