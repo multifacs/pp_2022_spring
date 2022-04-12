6dba28d613853e6a0fe63586ade34a3ab52f53ae
Nazarov69 <nniks1837@mail.ru>
2022-04-12 18:46:52 +0300

    simpson_omp

--- modules/task_2/nazarov_n_simpson/simpson.h
+++ modules/task_2/nazarov_n_simpson/simpson.h
@@ -1,6 +1,6 @@
 // Copyright 2022 Nazarov Nikita
-#ifndef MODULES_TASK_1_NAZAROV_N_SIMPSON_SIMPSON_H_
-#define MODULES_TASK_1_NAZAROV_N_SIMPSON_SIMPSON_H_
+#ifndef MODULES_TASK_2_NAZAROV_N_SIMPSON_SIMPSON_H_
+#define MODULES_TASK_2_NAZAROV_N_SIMPSON_SIMPSON_H_
 
 #include <algorithm>
 #include <cmath>
@@ -8,8 +8,10 @@
 #include <utility>
 #include <vector>
 
+#include "omp.h"
+
 double simpson(double (*func)(const std::vector<double>&),
                const std::vector<std::pair<double, double>>& borders,
                const std::vector<int>& steps);
 
-#endif  // MODULES_TASK_1_NAZAROV_N_SIMPSON_SIMPSON_H_
+#endif  // MODULES_TASK_2_NAZAROV_N_SIMPSON_SIMPSON_H_
