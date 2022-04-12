d986e2139ee3395447ac0c550e63da58a4b99175
Nazarov69 <nniks1837@mail.ru>
2022-04-12 18:00:40 +0300

    simpson

--- modules/task_1/nazarov_n_simpson/simpson.h
+++ modules/task_1/nazarov_n_simpson/simpson.h
@@ -2,13 +2,14 @@
 #ifndef MODULES_TASK_3_NAZAROV_N_SIMPSON_SIMPSON_H_
 #define MODULES_TASK_3_NAZAROV_N_SIMPSON_SIMPSON_H_
 
+#include <algorithm>
 #include <cmath>
 #include <functional>
-#include <vector>
-#include <algorithm>
 #include <utility>
+#include <vector>
 
-double simpson(double (*func)(const std::vector<double>&), const std::vector<std::pair<double, double>>& borders, const std::vector<int>& steps);
-
+double simpson(double (*func)(const std::vector<double>&),
+               const std::vector<std::pair<double, double>>& borders,
+               const std::vector<int>& steps);
 
 #endif  // MODULES_TASK_3_NAZAROV_N_SIMPSON_SIMPSON_H_
