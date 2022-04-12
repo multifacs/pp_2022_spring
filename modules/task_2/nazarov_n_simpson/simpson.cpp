c706a7e77c6ab76f040a5873941df63027d3316a
Nazarov69 <nniks1837@mail.ru>
2022-04-12 19:19:46 +0300

    simpson_omp

--- modules/task_2/nazarov_n_simpson/simpson.cpp
+++ modules/task_2/nazarov_n_simpson/simpson.cpp
@@ -25,30 +25,28 @@ double simpson(double (*func)(const std::vector<double>&),
   // omp_set_num_threads(12);
 
 #pragma omp parallel shared(dimensions, iteration, h, borders, steps, a) private(i, j) \
-  firstprivate(parameters, argument) reduction(+ : integral_simpson) // num_threads(12)
-  {
+  firstprivate(parameters, argument) reduction(+ : integral_simpson)  // num_threads(12)
 #pragma omp for schedule(dynamic)
-    for (i = 0; i < iteration; i++) {
-      x = i;
-      for (int dim = 0; dim < dimensions; dim++) {
-        parameters[dim][0] = borders[dim].first + (x % steps[dim]) * h[dim];
-        parameters[dim][1] =
-            borders[dim].first + (x % steps[dim]) * h[dim] + h[dim] / 2;
-        parameters[dim][2] = parameters[dim][1];
-        parameters[dim][3] = parameters[dim][1];
-        parameters[dim][4] = parameters[dim][1];
-        parameters[dim][5] = parameters[dim][0] + h[dim];
-        x /= steps[dim];
-      }
+  for (i = 0; i < iteration; i++) {
+    x = i;
+    for (int dim = 0; dim < dimensions; dim++) {
+      parameters[dim][0] = borders[dim].first + (x % steps[dim]) * h[dim];
+      parameters[dim][1] =
+          borders[dim].first + (x % steps[dim]) * h[dim] + h[dim] / 2;
+      parameters[dim][2] = parameters[dim][1];
+      parameters[dim][3] = parameters[dim][1];
+      parameters[dim][4] = parameters[dim][1];
+      parameters[dim][5] = parameters[dim][0] + h[dim];
+      x /= steps[dim];
+    }
 
-      for (j = 0; j < a; j++) {
-        x = j;
-        for (int dim = 0; dim < dimensions; dim++) {
-          argument[dim] = parameters[dim][x % 6];
-          x /= 6;
-        }
-        integral_simpson += func(argument);
+    for (j = 0; j < a; j++) {
+      x = j;
+      for (int dim = 0; dim < dimensions; dim++) {
+        argument[dim] = parameters[dim][x % 6];
+        x /= 6;
       }
+      integral_simpson += func(argument);
     }
   }
 
