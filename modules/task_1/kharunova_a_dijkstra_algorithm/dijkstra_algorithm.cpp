d55a600795999e4ad91c8b4fd11b1c75e0112e21
Zhigulyaka <alina.harunova@mail.ru>
2022-04-13 10:02:44 +0300

    refact

--- modules/task_1/kharunova_a_dijkstra_algorithm/dijkstra_algorithm.cpp
+++ modules/task_1/kharunova_a_dijkstra_algorithm/dijkstra_algorithm.cpp
@@ -1,18 +1,17 @@
 // Copyright 2022 Kharunova Alina
+#include <vector>
 #include <random>
 #include <algorithm>
 #include <iostream>
 #include "../../../modules/task_1/kharunova_a_dijkstra_algorithm/dijkstra_algorithm.h"
 
-int** create_random_graph(int num_of_vertex, int max_destination) {
+std::vector<std::vector<int>> create_random_graph(int num_of_vertex,
+                                                  int max_destination) {
     if (num_of_vertex <= 0) {
       throw "Wrong number of vertexes";
     }
-    int** matrix_of_connection;
-    matrix_of_connection = new int*[num_of_vertex];
-    for (int i = 0; i < num_of_vertex; i++) {
-      matrix_of_connection[i] = new int[num_of_vertex];
-    }
+    std::vector<std::vector<int>> matrix_of_connection(
+        num_of_vertex, std::vector<int>(num_of_vertex, 0));
     std::random_device dev;
     std::mt19937 gen(dev());
     int isConnected = 0;
@@ -20,7 +19,6 @@ int** create_random_graph(int num_of_vertex, int max_destination) {
     int destination = 0;
     for (int row = 0; row < num_of_vertex; row++) {
       for (int col = startCol; col < num_of_vertex; col++) {
-        matrix_of_connection[row][col] = 0;
         isConnected = dev() % 2;
         if (isConnected && row != col) {
           destination = dev() % max_destination;
@@ -44,16 +42,12 @@ void start_algorithm(int start_vertex, int num_of_vertex, int max_destination) {
     throw("Wrong value of maximum destination");
   }
 
-  int* min_destinations = new int[num_of_vertex];
-  int* passed_vertexes = new int[num_of_vertex];
+  std::vector<int> min_destinations(num_of_vertex, 1000);
+  std::vector<int> passed_vertexes(num_of_vertex, 1);
   int temp, min_index, min_dest;
-  int** matrix_of_connections =
+  std::vector<std::vector<int>> matrix_of_connections =
       create_random_graph(num_of_vertex, max_destination);
 
-  for (int i = 0; i < num_of_vertex; i++) {
-    min_destinations[i] = 1000;
-    passed_vertexes[i] = 1;
-  }
   min_destinations[start_vertex] = 0;
 
   do {
@@ -79,11 +73,9 @@ void start_algorithm(int start_vertex, int num_of_vertex, int max_destination) {
     }
   } while (min_index < 10000);
 
-  int* shortest_path = new int[num_of_vertex];
+  std::vector<int> shortest_path;
   int end_vertex = num_of_vertex - 1;
-  int last_vertex = 0;
-  shortest_path[last_vertex] = end_vertex;
-  last_vertex++;
+  shortest_path.push_back(end_vertex);
   int weight_of_end_vertex = min_destinations[end_vertex];
 
   while (end_vertex != 0) {
@@ -91,22 +83,12 @@ void start_algorithm(int start_vertex, int num_of_vertex, int max_destination) {
       if (matrix_of_connections[i][end_vertex] != 0) {
         int weight_of_last_vertex =
             weight_of_end_vertex - matrix_of_connections[i][end_vertex];
-   
         if (weight_of_last_vertex == min_destinations[i]) {
           weight_of_end_vertex = weight_of_last_vertex;
           end_vertex = i;
-          shortest_path[last_vertex] = i;
-          last_vertex++;
+          shortest_path.push_back(i);
         }
       }
     }
   }
-
-  delete[] shortest_path;
-  delete[] min_destinations;
-  delete[] passed_vertexes;
-  for (int i = 0; i < num_of_vertex; i++) {
-    delete[] matrix_of_connections[i];
-  }
-  delete[] matrix_of_connections;
 }
