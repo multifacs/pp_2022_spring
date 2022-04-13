d1ee55c198a8932e34e49554d297ceae8dd6292d
NovozhilovAY <sasha.n191@yandex.ru>
2022-04-13 13:24:16 +0300

    update tests

--- modules/task_2/novozhilov_a_matrix_multiplication/main.cpp
+++ modules/task_2/novozhilov_a_matrix_multiplication/main.cpp
@@ -4,29 +4,29 @@
 #include <vector>
 #include "./matrix_mult.h"
 
-TEST(Matrix_Multiplication_Seq, get_works) {
+TEST(Matrix_Multiplication_OMP, get_works) {
     SparseMatrix matrix(3, 3);
     EXPECT_NO_THROW(matrix.get(1, 1));
 }
 
-TEST(Matrix_Multiplication_Seq, get_throws_exception) {
+TEST(Matrix_Multiplication_OMP, get_throws_exception) {
     SparseMatrix matrix(3, 3);
     EXPECT_ANY_THROW(matrix.get(4, 4));
 }
 
-TEST(Matrix_Multiplication_Seq, multiplication_works) {
+TEST(Matrix_Multiplication_OMP, multiplication_works) {
     SparseMatrix matrix1(3, 5);
     SparseMatrix matrix2(5, 6);
     EXPECT_NO_THROW(matrix1.multiply_seq(matrix2));
 }
 
-TEST(Matrix_Multiplication_Seq, multiplication_throws_exception) {
+TEST(Matrix_Multiplication_OMP, multiplication_throws_exception) {
     SparseMatrix matrix1(3, 6);
     SparseMatrix matrix2(5, 6);
     EXPECT_ANY_THROW(matrix1.multiply_seq(matrix2));
 }
 
-TEST(Matrix_Multiplication_Seq, result_matrix_has_correct_size) {
+TEST(Matrix_Multiplication_OMP, result_matrix_has_correct_size) {
     SparseMatrix matrix1(3, 6);
     SparseMatrix matrix2(6, 10);
     SparseMatrix result = matrix1.multiply_seq(matrix2);
@@ -34,7 +34,7 @@ TEST(Matrix_Multiplication_Seq, result_matrix_has_correct_size) {
     EXPECT_EQ(result.getN(), matrix2.getN());
 }
 
-TEST(Matrix_Multiplication_Parallel, parallel_multiplication_returns_same_result) {
+TEST(Matrix_Multiplication_OMP, parallel_multiplication_returns_same_result) {
     SparseMatrix matrix1(3, 6);
     SparseMatrix matrix2(6, 10);
     SparseMatrix result1 = matrix1.multiply_parallel(matrix2);
@@ -42,7 +42,7 @@ TEST(Matrix_Multiplication_Parallel, parallel_multiplication_returns_same_result
     ASSERT_TRUE(result1 == result2);
 }
 
-// TEST(Matrix_Multiplication_Parallel, parallel_multiplication_works) {
+// TEST(Matrix_Multiplication_OMP, parallel_multiplication_works) {
 //    SparseMatrix matrix1(100, 100);
 //    SparseMatrix matrix2(100, 100);
 //    double startSeq = omp_get_wtime();
