fd1f389dbb080969de779534cd85a6120c10363f
Nikita Rodionov <nikita.rodionov@honeywell.com>
2022-04-14 17:35:48 +0300

    Fixed unused variables

--- modules/task_1/rodionov_n_linked_areas_lookup/main.cpp
+++ modules/task_1/rodionov_n_linked_areas_lookup/main.cpp
@@ -13,57 +13,29 @@ TEST(LinkedAreasLookup, GenerateBinaryImageTest1) {
     BinaryImage image = GenerateBinrayImage(IMAGE_SIZE1);
 
     ASSERT_EQ(image.size, IMAGE_SIZE1);
-    // Try to read all values, to check wheter array is okay
-    for (int x = 0; x < image.size; x++)
-        for (int y = 0; y < image.size; y++)
-            bool val = image.Get(x, y);
 }
 TEST(LinkedAreasLookup, FindAreasTest1) {
     BinaryImage image = GenerateBinrayImage(IMAGE_SIZE1);
     BinaryImageAreas* areas = FindAreas(image);
-
-    // Try to read all values, to check wheter array is okay
-    for (int x = 0; x < areas->size; x++)
-        for (int y = 0; y < areas->size; y++)
-            int val = areas->Get(x, y);
 }
 
 TEST(LinkedAreasLookup, GenerateBinaryImageTest2) {
     BinaryImage image = GenerateBinrayImage(IMAGE_SIZE2);
 
     ASSERT_EQ(image.size, IMAGE_SIZE2);
-    // Try to read all values, to check wheter array is okay
-    for (int x = 0; x < image.size; x++)
-        for (int y = 0; y < image.size; y++)
-            bool val = image.Get(x, y);
 }
 TEST(LinkedAreasLookup, FindAreasTest2) {
     BinaryImage image = GenerateBinrayImage(IMAGE_SIZE2);
     BinaryImageAreas* areas = FindAreas(image);
-
-    // Try to read all values, to check wheter array is okay
-    for (int x = 0; x < areas->size; x++)
-        for (int y = 0; y < areas->size; y++)
-            int val = areas->Get(x, y);
 }
 
 TEST(LinkedAreasLookup, GenerateBinaryImageTest3) {
     BinaryImage image = GenerateBinrayImage(IMAGE_SIZE3);
-
     ASSERT_EQ(image.size, IMAGE_SIZE1);
-    // Try to read all values, to check wheter array is okay
-    for (int x = 0; x < image.size; x++)
-        for (int y = 0; y < image.size; y++)
-            bool val = image.Get(x, y);
 }
 TEST(LinkedAreasLookup, FindAreasTest3) {
     BinaryImage image = GenerateBinrayImage(IMAGE_SIZE3);
     BinaryImageAreas* areas = FindAreas(image);
-
-    // Try to read all values, to check wheter array is okay
-    for (int x = 0; x < areas->size; x++)
-        for (int y = 0; y < areas->size; y++)
-            int val = areas->Get(x, y);
 }
 
 int main(int argc, char** argv) {
