53b895b751dda69c3aae868e3aba4ba75a7dff79
NickitaKraev <75929318+NickitaKraev@users.noreply.github.com>
2022-04-14 22:00:49 +0300

    fix

--- modules/task_1/kraev_n_radix_sort_with_even_odd_merge/radix_sort_with_even_odd_merge.h
+++ modules/task_1/kraev_n_radix_sort_with_even_odd_merge/radix_sort_with_even_odd_merge.h
@@ -1,12 +1,12 @@
 // Copyright 2022 Kraev Nikita
-#ifndef MODULES_TASK_1_KRAEV_N_RADIX_SORT_WITH_EVEN_ODD_MERGE_H_
-#define MODULES_TASK_1_KRAEV_N_RADIX_SORT_WITH_EVEN_ODD_MERGE_H_
+#ifndef MODULES_TASK_1_KRAEV_N_RADIX_SORT_WITH_EVEN_ODD_MERGE_RADIX_SORT_WITH_EVEN_ODD_MERGE_H_
+#define MODULES_TASK_1_KRAEV_N_RADIX_SORT_WITH_EVEN_ODD_MERGE_RADIX_SORT_WITH_EVEN_ODD_MERGE_H_
 #include <iostream>
 #include <vector>
 #include <random>
 #include <chrono>
 
 std::vector<double> getRandomVector(int size);
-void radixSort(std::vector<double>& vec);
+void radixSort(std::vector<double>& t);
 
 #endif  // MODULES_TASK_1_KRAEV_N_RADIX_SORT_WITH_EVEN_ODD_MERGE_RADIX_SORT_WITH_EVEN_ODD_MERGE_H_
