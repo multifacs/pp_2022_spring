64414af1fc63c034ec909bf4c0b5270e8abc51e1
NickitaKraev <75929318+NickitaKraev@users.noreply.github.com>
2022-04-14 21:49:19 +0300

    fix codestyle

--- modules/task_1/kraev_n_radix_sort_with_even_odd_merge/radix_sort_with_even_odd_merge.h
+++ modules/task_1/kraev_n_radix_sort_with_even_odd_merge/radix_sort_with_even_odd_merge.h
@@ -9,4 +9,4 @@
 std::vector<double> getRandomVector(int size);
 void radixSort(std::vector<double>& vec);
 
-#endif  // MODULES_TASK_1_KRAEV_N_RADIX_SORT_WITH_EVEN_ODD_MERGE_H_
+#endif  // MODULES_TASK_1_KRAEV_N_RADIX_SORT_WITH_EVEN_ODD_MERGE_RADIX_SORT_WITH_EVEN_ODD_MERGE_H_
