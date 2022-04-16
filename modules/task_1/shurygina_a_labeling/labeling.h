66907112afa167cfc02dfd5ce2ebbeecc5d08581
ShuryginaA <annashurygina353@gmail.com>
2022-04-16 14:41:53 +0300

    test

--- modules/task_1/shurygina_a_labeling/labeling.h
+++ modules/task_1/shurygina_a_labeling/labeling.h
@@ -9,7 +9,8 @@
 std::vector<int> getRandomImg(int rows, int cols);
 std::pair<std::vector<int>, std::vector<int> > 
          firstPass(std::vector<int>, int, int);
-std::vector<int> secondPass(std::vector<int>, int, int, std::vector<int>);
+std::vector<int> 
+         secondPass(std::vector<int>, int, int, std::vector<int>);
 std::vector<int> labeling(const std::vector<int>&, int, int);
 
 #endif  //  MODULES_TASK_1_SHURYGINA_A_LABELING_LABELING_H_
