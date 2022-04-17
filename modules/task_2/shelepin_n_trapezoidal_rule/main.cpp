02e7204208ca23df5facd8c16fc03ecde0c8dc3c
Nick Shell <kiwi0tails@gmail.com>
2022-04-17 12:06:45 +0300

    fixed naming

--- modules/task_2/shelepin_n_trapezoidal_rule/main.cpp
+++ modules/task_2/shelepin_n_trapezoidal_rule/main.cpp
@@ -5,7 +5,7 @@
 
 const double eps = 100;
 
-TEST(TRAPEZOIDAL_RULE_MPI, TEST_DIM_1_FUNC_1) {
+TEST(TRAPEZOIDAL_RULE_OMP, TEST_DIM_1_FUNC_1) {
   std::vector<std::pair<double, double>> limits(2);
   limits = {{-1, 5}};
   int n = 300;
@@ -20,7 +20,7 @@ TEST(TRAPEZOIDAL_RULE_MPI, TEST_DIM_1_FUNC_1) {
   const double ref_result = 156;
   seq_result = getParallelTrapezoidal(n, limits, f);
 
-  std::cout << "Seq. result: " << seq_result << std::fixed
+  std::cout << "Par. result: " << seq_result << std::fixed
             << " | Ref. result: " << ref_result << std::fixed
             << " | Error: " << abs(ref_result - seq_result) << std::fixed
             << std::endl;
@@ -28,7 +28,7 @@ TEST(TRAPEZOIDAL_RULE_MPI, TEST_DIM_1_FUNC_1) {
   ASSERT_NEAR(seq_result, ref_result, eps);
 }
 
-TEST(TRAPEZOIDAL_RULE_MPI, TEST_DIM_2_FUNC_2) {
+TEST(TRAPEZOIDAL_RULE_OMP, TEST_DIM_2_FUNC_2) {
   std::vector<std::pair<double, double>> limits(2);
   limits = {{0, 2}, {0, 1}};
   int n = 200;
@@ -44,7 +44,7 @@ TEST(TRAPEZOIDAL_RULE_MPI, TEST_DIM_2_FUNC_2) {
   const double ref_result = 2.666;
   seq_result = getParallelTrapezoidal(n, limits, f);
 
-  std::cout << "Seq. result: " << seq_result << std::fixed
+  std::cout << "Par. result: " << seq_result << std::fixed
             << " | Ref. result: " << ref_result << std::fixed
             << " | Error: " << abs(ref_result - seq_result) << std::fixed
             << std::endl;
@@ -52,7 +52,7 @@ TEST(TRAPEZOIDAL_RULE_MPI, TEST_DIM_2_FUNC_2) {
   ASSERT_NEAR(seq_result, ref_result, eps);
 }
 
-TEST(TRAPEZOIDAL_RULE_MPI, TEST_DIM_2_FUNC_3) {
+TEST(TRAPEZOIDAL_RULE_OMP, TEST_DIM_2_FUNC_3) {
   std::vector<std::pair<double, double>> limits(2);
   limits = {{0, 5}, {3, 7}};
   int n = 100;
@@ -68,7 +68,7 @@ TEST(TRAPEZOIDAL_RULE_MPI, TEST_DIM_2_FUNC_3) {
   const double ref_result = 200;
   seq_result = getParallelTrapezoidal(n, limits, f);
 
-  std::cout << "Seq. result: " << seq_result << std::fixed
+  std::cout << "Par. result: " << seq_result << std::fixed
             << " | Ref. result: " << ref_result << std::fixed
             << " | Error: " << abs(ref_result - seq_result) << std::fixed
             << std::endl;
@@ -76,7 +76,7 @@ TEST(TRAPEZOIDAL_RULE_MPI, TEST_DIM_2_FUNC_3) {
   ASSERT_NEAR(seq_result, ref_result, eps);
 }
 
-TEST(TRAPEZOIDAL_RULE_MPI, TEST_DIM_3_FUNC_4) {
+TEST(TRAPEZOIDAL_RULE_OMP, TEST_DIM_3_FUNC_4) {
   std::vector<std::pair<double, double>> limits(2);
   limits = {{0, 1}, {-4, 2}, {5, 7}};
   int n = 20;
@@ -93,7 +93,7 @@ TEST(TRAPEZOIDAL_RULE_MPI, TEST_DIM_3_FUNC_4) {
   const double ref_result = -2671.297;
   seq_result = getParallelTrapezoidal(n, limits, f);
 
-  std::cout << "Seq. result: " << seq_result << std::fixed
+  std::cout << "Par. result: " << seq_result << std::fixed
             << " | Ref. result: " << ref_result << std::fixed
             << " | Error: " << abs(ref_result - seq_result) << std::fixed
             << std::endl;
@@ -101,7 +101,7 @@ TEST(TRAPEZOIDAL_RULE_MPI, TEST_DIM_3_FUNC_4) {
   ASSERT_NEAR(seq_result, ref_result, eps);
 }
 
-TEST(TRAPEZOIDAL_RULE_MPI, TEST_DIM_3_FUNC_5) {
+TEST(TRAPEZOIDAL_RULE_OMP, TEST_DIM_3_FUNC_5) {
   std::vector<std::pair<double, double>> limits(2);
   limits = {{1, 3}, {5, 8}, {10, 12}};
   int n = 100;
@@ -118,7 +118,7 @@ TEST(TRAPEZOIDAL_RULE_MPI, TEST_DIM_3_FUNC_5) {
   const double ref_result = -0.1;
   seq_result = getParallelTrapezoidal(n, limits, f);
 
-  std::cout << "Seq. result: " << seq_result << std::fixed
+  std::cout << "Par. result: " << seq_result << std::fixed
             << " | Ref. result: " << ref_result << std::fixed
             << " | Error: " << abs(ref_result - seq_result) << std::fixed
             << std::endl;
