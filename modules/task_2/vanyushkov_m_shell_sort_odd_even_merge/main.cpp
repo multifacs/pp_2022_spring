e9e8fc39fa2e75714e06c21e6571497c18776896
MaksaLegend <61109606+VanyushkovMM@users.noreply.github.com>
2022-04-18 19:56:23 +0300

    update tests

--- modules/task_2/vanyushkov_m_shell_sort_odd_even_merge/main.cpp
+++ modules/task_2/vanyushkov_m_shell_sort_odd_even_merge/main.cpp
@@ -6,17 +6,17 @@ bool test(int size) {
     std::vector<int> vec_seq = getRandomVector(size);
     std::vector<int> vec_par = vec_seq;
 
-    // volatile double start_seq = omp_get_wtime();
+    volatile double start_seq = omp_get_wtime();
     ShellSortSequantial(&vec_seq);
-    // volatile double end_seq = omp_get_wtime();
+    volatile double end_seq = omp_get_wtime();
 
-    // volatile double start_par = omp_get_wtime();
+    volatile double start_par = omp_get_wtime();
     ShellSortParallel(&vec_par);
-    // volatile double end_par = omp_get_wtime();
+    volatile double end_par = omp_get_wtime();
 
-    // double seq_time = end_seq - start_seq;
-    // double par_time = end_par - start_par;
-    // std::cout << "Effective: " << seq_time / par_time << '\n';
+    double seq_time = end_seq - start_seq;
+    double par_time = end_par - start_par;
+    std::cout << "Effective: " << seq_time / par_time << '\n';
 
     return vec_seq == vec_par && checkSort(vec_par);
 }
@@ -25,20 +25,20 @@ TEST(ShellSort, test_size_101) {
     ASSERT_TRUE(test(101));
 }
 
-TEST(ShellSort, test_size_1503) {
-    ASSERT_TRUE(test(1503));
+TEST(ShellSort, test_size_503) {
+    ASSERT_TRUE(test(503));
 }
 
-TEST(ShellSort, test_size_10021) {
-    ASSERT_TRUE(test(10021));
+TEST(ShellSort, test_size_1021) {
+    ASSERT_TRUE(test(1021));
 }
 
-TEST(ShellSort, test_size_32322) {
-    ASSERT_TRUE(test(32322));
+TEST(ShellSort, test_size_2322) {
+    ASSERT_TRUE(test(2322));
 }
 
-TEST(ShellSort, test_size_132322) {
-    ASSERT_TRUE(test(132322));
+TEST(ShellSort, test_size_12323) {
+    ASSERT_TRUE(test(12323));
 }
 
 int main(int argc, char **argv) {
