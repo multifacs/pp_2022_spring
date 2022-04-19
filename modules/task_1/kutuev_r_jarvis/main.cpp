7d1b60298b1fc37d45c3ffbb1c97b98f01b80bcb
KutuevR <roman.kutuev2001@icloud.com>
2022-04-19 19:50:26 +0300

    jarviss

--- modules/task_1/kutuev_r_jarvis/main.cpp
+++ modules/task_1/kutuev_r_jarvis/main.cpp
@@ -1,11 +1,11 @@
 // Copyright 2022 Kutuev Roman
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
 #include "../../../modules/task_1/kutuev_r_jarvis/jarvis.h"
 TEST(Jarvis, test_1) {
   jarvis quick_hull;
   std::vector<Point> quick = quick_hull.convex_hull(quick_hull.set_points(2));
-  ASSERT_EQ(quick.size(), 0);
+  EXPECT_DOUBLE_EQ(quick.size(), 0);
 }
 
 TEST(Jarvis, test_2) {
@@ -13,28 +13,28 @@ TEST(Jarvis, test_2) {
   std::vector<Point> polygon{{1, 1}, {2, 2}, {2, 1}, {1, 2}};
   std::vector<Point> quick = quick_hull.convex_hull(polygon);
 
-  ASSERT_EQ(quick[0].x, polygon[0].x);
-  ASSERT_EQ(quick[0].y, polygon[0].y);
-  ASSERT_EQ(quick[1].x, polygon[2].x);
-  ASSERT_EQ(quick[1].y, polygon[2].y);
-  ASSERT_EQ(quick[2].x, polygon[1].x);
-  ASSERT_EQ(quick[2].y, polygon[1].y);
-  ASSERT_EQ(quick[3].x, polygon[3].x);
-  ASSERT_EQ(quick[3].y, polygon[3].y);
+  EXPECT_DOUBLE_EQ(quick[0].x, polygon[0].x);
+  EXPECT_DOUBLE_EQ(quick[0].y, polygon[0].y);
+  EXPECT_DOUBLE_EQ(quick[1].x, polygon[2].x);
+  EXPECT_DOUBLE_EQ(quick[1].y, polygon[2].y);
+  EXPECT_DOUBLE_EQ(quick[2].x, polygon[1].x);
+  EXPECT_DOUBLE_EQ(quick[2].y, polygon[1].y);
+  EXPECT_DOUBLE_EQ(quick[3].x, polygon[3].x);
+  EXPECT_DOUBLE_EQ(quick[3].y, polygon[3].y);
 }
 TEST(Jarvis, test_3) {
   jarvis quick_hull;
   std::vector<Point> polygon{{1, 1}, {3, 1}, {3, 2}, {3, 3}, {2, 3}, {1, 3}};
   std::vector<Point> quick = quick_hull.convex_hull(polygon);
 
-  ASSERT_EQ(quick[0].x, polygon[0].x);
-  ASSERT_EQ(quick[0].y, polygon[0].y);
-  ASSERT_EQ(quick[1].x, polygon[1].x);
-  ASSERT_EQ(quick[1].y, polygon[1].y);
-  ASSERT_EQ(quick[2].x, polygon[3].x);
-  ASSERT_EQ(quick[2].y, polygon[3].x);
-  ASSERT_EQ(quick[3].x, polygon[5].x);
-  ASSERT_EQ(quick[3].y, polygon[5].y);
+  EXPECT_DOUBLE_EQ(quick[0].x, polygon[0].x);
+  EXPECT_DOUBLE_EQ(quick[0].y, polygon[0].y);
+  EXPECT_DOUBLE_EQ(quick[1].x, polygon[1].x);
+  EXPECT_DOUBLE_EQ(quick[1].y, polygon[1].y);
+  EXPECT_DOUBLE_EQ(quick[2].x, polygon[3].x);
+  EXPECT_DOUBLE_EQ(quick[2].y, polygon[3].x);
+  EXPECT_DOUBLE_EQ(quick[3].x, polygon[5].x);
+  EXPECT_DOUBLE_EQ(quick[3].y, polygon[5].y);
 }
 TEST(Jarvis, test_4) {
   jarvis quick_hull;
@@ -42,20 +42,20 @@ TEST(Jarvis, test_4) {
                              {228, 309}, {449, 247}, {260, 98},  {264, 368},
                              {208, 286}, {365, 0}};
   std::vector<Point> quick = quick_hull.convex_hull(polygon);
-  ASSERT_EQ(quick[0].x, polygon[2].x);
-  ASSERT_EQ(quick[0].y, polygon[2].y);
-  ASSERT_EQ(quick[1].x, polygon[3].x);
-  ASSERT_EQ(quick[1].y, polygon[3].y);
-  ASSERT_EQ(quick[2].x, polygon[9].x);
-  ASSERT_EQ(quick[2].y, polygon[9].y);
-  ASSERT_EQ(quick[3].x, polygon[5].x);
-  ASSERT_EQ(quick[3].y, polygon[5].y);
-  ASSERT_EQ(quick[4].x, polygon[1].x);
-  ASSERT_EQ(quick[4].y, polygon[1].y);
-  ASSERT_EQ(quick[5].x, polygon[7].x);
-  ASSERT_EQ(quick[5].y, polygon[7].y);
-  ASSERT_EQ(quick[6].x, polygon[8].x);
-  ASSERT_EQ(quick[6].y, polygon[8].y);
+  EXPECT_DOUBLE_EQ(quick[0].x, polygon[2].x);
+  EXPECT_DOUBLE_EQ(quick[0].y, polygon[2].y);
+  EXPECT_DOUBLE_EQ(quick[1].x, polygon[3].x);
+  EXPECT_DOUBLE_EQ(quick[1].y, polygon[3].y);
+  EXPECT_DOUBLE_EQ(quick[2].x, polygon[9].x);
+  EXPECT_DOUBLE_EQ(quick[2].y, polygon[9].y);
+  EXPECT_DOUBLE_EQ(quick[3].x, polygon[5].x);
+  EXPECT_DOUBLE_EQ(quick[3].y, polygon[5].y);
+  EXPECT_DOUBLE_EQ(quick[4].x, polygon[1].x);
+  EXPECT_DOUBLE_EQ(quick[4].y, polygon[1].y);
+  EXPECT_DOUBLE_EQ(quick[5].x, polygon[7].x);
+  EXPECT_DOUBLE_EQ(quick[5].y, polygon[7].y);
+  EXPECT_DOUBLE_EQ(quick[6].x, polygon[8].x);
+  EXPECT_DOUBLE_EQ(quick[6].y, polygon[8].y);
 }
 
 TEST(Jarvis, test_5) {
