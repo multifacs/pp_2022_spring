a5179a54a9a4cc54422bc98c952c131a8a8cf3b1
Vodeneev <55285701+Vodeneev@users.noreply.github.com>
2022-04-21 00:49:10 +0300

    First commit

---
+++ modules/task_1/vodeneev_m_jarvis/jarvis.h
@@ -0,0 +1,16 @@
+// Copyright 2022 VODENEEV MIKHAIL
+#ifndef MODULES_TASK_1_VODENEEV_M_JARVIS_JARVIS_H_
+#define MODULES_TASK_1_VODENEEV_M_JARVIS_JARVIS_H_
+#include <random>
+#include <vector>
+
+std::vector<std::pair<double, double>> GetPoints(int left_border, int right_border,
+    int low_border, int high_border, int size);
+
+double OrientationPointRelativeToVector(std::pair<double, double> A,
+    std::pair<double, double> B, std::pair<double, double> point);
+
+std::vector<std::pair<double, double>> JarvisSeq(std::vector<std::pair
+    <double, double>> points);
+
+#endif  // MODULES_TASK_1_VODENEEV_M_JARVIS_JARVIS_H_
