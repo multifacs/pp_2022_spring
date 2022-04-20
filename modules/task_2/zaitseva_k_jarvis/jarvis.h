2d0c951e2476617f94fcd5a66737a3f4eaac1697
Kcenia <Kcenia4010@gmail.com>
2022-04-20 19:27:41 +0300

    Initial commit

---
+++ modules/task_2/zaitseva_k_jarvis/jarvis.h
@@ -0,0 +1,33 @@
+// Copyright 2022 Zaitseva Ksenia
+#ifndef MODULES_TASK_2_ZAITSEVA_KSENIA_JARVIS_H_
+#define MODULES_TASK_2_ZAITSEVA_KSENIA_JARVIS_H_
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <random>
+#include <omp.h>
+
+struct Point {
+  Point(double x_ = 0.0, double y_ = 0.0) : x(x_), y(y_){};
+  double x;
+  double y;
+  bool operator<(const Point& other) const;
+  bool operator==(const Point& other) const;
+  bool operator!=(const Point& other) const;
+  Point operator-(const Point& other) const;
+};
+
+double cross(const Point& a, const Point& b);
+
+bool turn(const Point& a, const Point& b,
+          const Point& c);
+
+std::vector<Point> Jarvis(std::vector<Point> points);
+
+std::vector<Point> Generate(int n);
+
+std::vector<Point> Jarvis_parallel(std::vector<Point> points);
+
+#endif  // MODULES_TASK_2_ZAITSEVA_KSENIA_JARVIS_H_
