8b445d0b3251cdadfdef51ebcf1d1faa19163b74
AndrewGudkov <63961057+AndrewGudkov@users.noreply.github.com>
2022-04-20 23:24:40 +0300

    style

--- modules/task_3/gudkov_a_gaussian_hor/main.cpp
+++ modules/task_3/gudkov_a_gaussian_hor/main.cpp
@@ -1,6 +1,7 @@
 // Copyright 2022 Gudkov Andrey
-#include "./gaussian.h"
+
 #include <gtest/gtest.h>
+#include "./gaussian.h"
 
 TEST(Gauss_Filter, _50x50) {
     int width = 50;
@@ -61,23 +62,24 @@ TEST(Gauss_Filter, _500x500) {
     par = ParFilter(img, width, height, 32);
     ASSERT_EQ(seq, par);
 }
-//TEST(Gauss_Filter, _3000x5500)
-//{
-//    int width = 1000;
-//    int height = 1000;
-//    std::vector<uint8_t> img = GetRandMatrix(width, height);
-//    std::vector<uint8_t> seq;
-//    std::vector<uint8_t> par;
-//    tbb::tick_count omp_start = tbb::tick_count::now();
-//    seq = Filter(img, width, height, 32);
-//    tbb::tick_count omp_end = tbb::tick_count::now();
-//    tbb::tick_count seq_start = tbb::tick_count::now();
-//    par = ParFilter(img, width, height, 32);
-//    tbb::tick_count seq_end = tbb::tick_count::now();
-//    std::cout << "seq " << (seq_end - seq_start).seconds() << std::endl;
-//    std::cout << "tbb " << (omp_end - omp_start).seconds() << std::endl;
-//    ASSERT_EQ(seq, par);
-//}
+/*
+TEST(Gauss_Filter, _1000x1000)
+{
+    int width = 1000;
+    int height = 1000;
+    std::vector<uint8_t> img = GetRandMatrix(width, height);
+    std::vector<uint8_t> seq;
+    std::vector<uint8_t> par;
+    tbb::tick_count omp_start = tbb::tick_count::now();
+    seq = Filter(img, width, height, 32);
+    tbb::tick_count omp_end = tbb::tick_count::now();
+    tbb::tick_count seq_start = tbb::tick_count::now();
+    par = ParFilter(img, width, height, 32);
+    tbb::tick_count seq_end = tbb::tick_count::now();
+    std::cout << "seq " << (seq_end - seq_start).seconds() << std::endl;
+    std::cout << "tbb " << (omp_end - omp_start).seconds() << std::endl;
+    ASSERT_EQ(seq, par);
+}*/
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
