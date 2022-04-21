87594261c4451d55b5c46464cefd4442cb13038b
ShuryginaA <annashurygina353@gmail.com>
2022-04-21 18:44:03 +0300

    fix

--- modules/task_2/shurygina_a_labeling_omp/labeling.h
+++ modules/task_2/shurygina_a_labeling_omp/labeling.h
@@ -11,9 +11,9 @@ std::vector<int> secondMark(std::vector<int>, int, int, std::vector<int>);
 
 std::pair<std::vector<int>, std::vector<int> >
 firstMarkOmp(std::vector<int>, int, int);
-std::vector<int> 
+std::vector<int>
 CLabelingOmp(const std::vector<int>& array, int rows, int cols);
-std::vector<int> 
+std::vector<int>
 transformResult(const std::vector<int>& array, int rows, int cols);
 
 #endif  // MODULES_TASK_2_SHURYGINA_A_LABELING_OMP_LABELING_H_
