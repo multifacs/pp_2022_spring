db12acd365bf6cd43d998fc29fa64d437b8051b2
George Amberson <George1984Amberson@yandex.ru>
2022-04-23 18:15:09 +0300

    add extra test

--- modules/task_3/kulemin_p_discharge_double_sort_tbb/main.cpp
+++ modules/task_3/kulemin_p_discharge_double_sort_tbb/main.cpp
@@ -3,6 +3,18 @@
 #include <vector>
 #include "./kulemin_p_discharge_double_sort_tbb.h"
 
+TEST(Parallel_Operations_OpenMP, Test_90000) {
+    int count = 90000;
+    vector* vb = create_random_vector(count);
+    vector* sd = new vector(count);
+    vector* tr = new vector(count);
+    copy_vectors(vb->ptr, sd->ptr, count);
+    discharge_sort_seq(vb);
+    discharge_sort(sd, tr);
+    bool res = check_vectors(vb->ptr, sd->ptr, count);
+    ASSERT_EQ(true, res);
+}
+
 TEST(Parallel_Operations_OpenMP, Test_100000) {
     int count = 100000;
     vector* vb = create_random_vector(count);
