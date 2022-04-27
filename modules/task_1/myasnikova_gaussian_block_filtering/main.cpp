695f82a732bb0d531f8604c184ff0f99a3fcc7e2
var2var3varvara_h <varyam6@gmail.com>
2022-04-27 14:52:05 +0300

    v1

--- modules/task_1/myasnikova_gaussian_block_filtering/main.cpp
+++ modules/task_1/myasnikova_gaussian_block_filtering/main.cpp
@@ -4,36 +4,36 @@
 #include <vector>
 #include "../../../modules/task_1/myasnikova_gaussian_block_filtering/gaussian_block_filtering.h"
 
-TEST(Sequential_gauss, Creating_a_Matrix_small) {
+TEST(SeqGaussian, Creating_a_Matrix_small) {
   int row = 10, column = 10;
   ASSERT_NO_THROW(CreateMatrix(row, column));
 }
 
-TEST(Sequential_gauss, Creating_a_Matrix_big) {
+TEST(SeqGaussian, Creating_a_Matrix_big) {
   int row = 100, column = 100;
   ASSERT_NO_THROW(CreateMatrix(row, column));
 }
 
-TEST(Sequential_gauss, Creating_a_Kernel_1) {
+TEST(SeqGaussian, Creating_a_Kernel_1) {
   ASSERT_NO_THROW(CreateKernel(1));
 }
 
-TEST(Sequential_gauss, Creating_a_Kernel_2) {
+TEST(SeqGaussian, Creating_a_Kernel_2) {
   ASSERT_NO_THROW(CreateKernel(5));
 }
 
-TEST(Sequential_gauss, Creating_a_Kernel_3) {
+TEST(SeqGaussian, Creating_a_Kernel_3) {
   ASSERT_NO_THROW(CreateKernel(10));
 }
 
-TEST(Sequential_gauss, GaussFilterSeq_small) {
+TEST(SeqGaussian, GaussFilterSeq_small) {
   int row = 10, column = 10;
   std::vector<int> matrix = CreateMatrix(row, column);
   std::vector<int> kernel = CreateKernel(5);
   ASSERT_NO_THROW(GaussFilterSeq(matrix, kernel, row, column));
 }
 
-TEST(Sequential_gauss, GaussFilterSeq_big) {
+TEST(SeqGaussian, GaussFilterSeq_big) {
   int row = 100, column = 100;
   std::vector<int> matrix = CreateMatrix(row, column);
   std::vector<int> kernel = CreateKernel(5);
