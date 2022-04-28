bc0a860554374114c0476b9ab18cead2cbaf56d2
Nick Shell <kiwi0tails@gmail.com>
2022-04-28 07:55:21 +0300

    strassen omp

--- modules/task_2/lazarev_a_strassen/strassen.cpp
+++ modules/task_2/lazarev_a_strassen/strassen.cpp
@@ -2,6 +2,8 @@
 
 #include "../../../modules/task_1/lazarev_a_strassen/strassen.h"
 
+#include <omp.h>
+
 #include <algorithm>
 #include <cmath>
 #include <iostream>
@@ -82,8 +84,6 @@ void Strassen(int n, const matrix& A, const matrix& B, matrix* C) {
          M5 = std::vector<vec>(n / 2, vec(n / 2, 0)),
          M6 = std::vector<vec>(n / 2, vec(n / 2, 0)),
          M7 = std::vector<vec>(n / 2, vec(n / 2, 0));
-  matrix AA = std::vector<vec>(n / 2, vec(n / 2, 0)),
-         BB = std::vector<vec>(n / 2, vec(n / 2, 0));
 
   if (n == 2) {
     matrixMultiplicationSimple(A, B, C);
@@ -102,36 +102,74 @@ void Strassen(int n, const matrix& A, const matrix& B, matrix* C) {
       }
     }
 
-    // Calculate M1 = (A0 + A3) * (B0 + B3)
-    matrixAdd(n / 2, A11, A22, &AA);
-    matrixAdd(n / 2, B11, B22, &BB);
-    Strassen(n / 2, AA, BB, &M1);
+#pragma omp parallel sections shared(A11, A12, A21, A22, B11, B12, B21, B22)
+    {
+#pragma omp section
+      {
+        matrix AA = std::vector<vec>(n / 2, vec(n / 2, 0)),
+               BB = std::vector<vec>(n / 2, vec(n / 2, 0));
 
-    // Calculate M2 = (A2 + A3) * B0
-    matrixAdd(n / 2, A21, A22, &AA);
-    Strassen(n / 2, AA, B11, &M2);
+        // Calculate M1 = (A0 + A3) * (B0 + B3)
+        matrixAdd(n / 2, A11, A22, &AA);
+        matrixAdd(n / 2, B11, B22, &BB);
+        Strassen(n / 2, AA, BB, &M1);
+      }
 
-    // Calculate M3 = A0 * (B1 - B3)
-    matrixSub(n / 2, B12, B22, &BB);
-    Strassen(n / 2, A11, BB, &M3);
+#pragma omp section
+      {
+        matrix AA = std::vector<vec>(n / 2, vec(n / 2, 0));
+        // Calculate M2 = (A2 + A3) * B0
+        matrixAdd(n / 2, A21, A22, &AA);
+        Strassen(n / 2, AA, B11, &M2);
+      }
 
-    // Calculate M4 = A3 * (B2 - B0)
-    matrixSub(n / 2, B21, B11, &BB);
-    Strassen(n / 2, A22, BB, &M4);
+#pragma omp section
+      {
+        matrix BB = std::vector<vec>(n / 2, vec(n / 2, 0));
+        // Calculate M3 = A0 * (B1 - B3)
+        matrixSub(n / 2, B12, B22, &BB);
+        Strassen(n / 2, A11, BB, &M3);
+      }
 
-    // Calculate M5 = (A0 + A1) * B3
-    matrixAdd(n / 2, A11, A12, &AA);
-    Strassen(n / 2, AA, B22, &M5);
+#pragma omp section
+      {
+        matrix BB = std::vector<vec>(n / 2, vec(n / 2, 0));
+        // Calculate M4 = A3 * (B2 - B0)
+        matrixSub(n / 2, B21, B11, &BB);
+        Strassen(n / 2, A22, BB, &M4);
+      }
 
-    // Calculate M6 = (A2 - A0) * (B0 + B1)
-    matrixSub(n / 2, A21, A11, &AA);
-    matrixAdd(n / 2, B11, B12, &BB);
-    Strassen(n / 2, AA, BB, &M6);
+#pragma omp section
+      {
+        matrix AA = std::vector<vec>(n / 2, vec(n / 2, 0));
+        // Calculate M5 = (A0 + A1) * B3
+        matrixAdd(n / 2, A11, A12, &AA);
+        Strassen(n / 2, AA, B22, &M5);
+      }
 
-    // Calculate M7 = (A1 - A3) * (B2 + B3)
-    matrixSub(n / 2, A12, A22, &AA);
-    matrixAdd(n / 2, B21, B22, &BB);
-    Strassen(n / 2, AA, BB, &M7);
+#pragma omp section
+      {
+        matrix AA = std::vector<vec>(n / 2, vec(n / 2, 0)),
+               BB = std::vector<vec>(n / 2, vec(n / 2, 0));
+        // Calculate M6 = (A2 - A0) * (B0 + B1)
+        matrixSub(n / 2, A21, A11, &AA);
+        matrixAdd(n / 2, B11, B12, &BB);
+        Strassen(n / 2, AA, BB, &M6);
+      }
+
+#pragma omp section
+      {
+        matrix AA = std::vector<vec>(n / 2, vec(n / 2, 0)),
+               BB = std::vector<vec>(n / 2, vec(n / 2, 0));
+        // Calculate M7 = (A1 - A3) * (B2 + B3)
+        matrixSub(n / 2, A12, A22, &AA);
+        matrixAdd(n / 2, B21, B22, &BB);
+        Strassen(n / 2, AA, BB, &M7);
+      }
+    }
+
+    matrix AA = std::vector<vec>(n / 2, vec(n / 2, 0)),
+           BB = std::vector<vec>(n / 2, vec(n / 2, 0));
 
     // Calculate C0 = M1 + M4 - M5 + M7
     matrixAdd(n / 2, M1, M4, &AA);
