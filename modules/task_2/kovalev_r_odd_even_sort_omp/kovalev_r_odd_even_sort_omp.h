13062838abce6eaeb210d34fb4894122b8cc8577
Ruslan <ruslan_kovalev_nn@mail.ru>
2022-04-28 15:38:10 +0300

    second lab work

---
+++ modules/task_2/kovalev_r_odd_even_sort_omp/kovalev_r_odd_even_sort_omp.h
@@ -0,0 +1,18 @@
+// Copyright 2022 Kovalev Ruslan
+#ifndef MODULES_TEST_TASKS_TEST_OMP_OPS_OMP_H_
+#define MODULES_TEST_TASKS_TEST_OMP_OPS_OMP_H_
+
+#include <string>
+#include <vector>
+
+bool check(std::vector<int>* arr_1, std::vector<int>* arr_2, int sz);
+
+void vec_gen(std::vector<int>* vec, int len);
+
+void copy_vectors(std::vector<int>* arr_1, std::vector<int>* arr_2, int sz);
+
+void getSequantialSort(std::vector<int>* arr, int sz);
+
+std::vector<int> getParallelSort(std::vector<int>& commonVector);
+
+#endif  // MODULES_TEST_TASKS_TEST_OMP_OPS_OMP_H_
