e0e086596f632a7aa4eaa5a1d2ab0733a5a668e7
Ruslan <ruslan_kovalev_nn@mail.ru>
2022-04-28 16:31:51 +0300

    fix 3

--- modules/task_2/kovalev_r_odd_even_sort_omp/kovalev_r_odd_even_sort_omp.h
+++ modules/task_2/kovalev_r_odd_even_sort_omp/kovalev_r_odd_even_sort_omp.h
@@ -12,4 +12,4 @@ void copy_vectors(std::vector<int>* arr_1, std::vector<int>* arr_2, int sz);
 void getSequantialSort(std::vector<int>* arr, int sz);
 std::vector<int> getParallelSort(const std::vector<int>& commonVector);
 
-#endif // MODULES_TASK_2_KOVALEV_R_ODD_EVEN_SORT_OMP_KOVALEV_R_ODD_EVEN_SORT_OMP_H_
+#endif  // MODULES_TASK_2_KOVALEV_R_ODD_EVEN_SORT_OMP_KOVALEV_R_ODD_EVEN_SORT_OMP_H_
