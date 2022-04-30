7768d39aa5f0510717823e86ef1e704879d7fae5
AntonPoplavsky <poplavskyanton@mail.ru>
2022-04-30 11:40:11 +0300

    sss

--- modules/task_1/poplavsky_a_bin_convex_hull/main.cpp
+++ modules/task_1/poplavsky_a_bin_convex_hull/main.cpp
@@ -22,3 +22,59 @@ TEST(ConvexHull, time) {
   printf("hull time : %lf\n\n",
          static_cast<float>(time2 - time1) / CLOCKS_PER_SEC);
 }
+
+TEST(ConvexHull, ttt) {
+  std::random_device dev;
+  std::mt19937 gen(dev());
+  std::uniform_real_distribution<> urd(-1000, 1000);
+  std::vector<Coordinates> points;
+  Coordinates pt;
+  for (int i = 0; i < 3; i++) {
+    pt.set_x(urd(gen));
+    pt.set_y(urd(gen));
+    points.push_back(pt);
+  }
+  std::vector<Coordinates> hull = graham(points);
+}
+
+TEST(ConvexHull, iii) {
+  std::random_device dev;
+  std::mt19937 gen(dev());
+  std::uniform_real_distribution<> urd(-1000, 1000);
+  std::vector<Coordinates> points;
+  Coordinates pt;
+  for (int i = 0; i < 5; i++) {
+    pt.set_x(urd(gen));
+    pt.set_y(urd(gen));
+    points.push_back(pt);
+  }
+  std::vector<Coordinates> hull = graham(points);
+}
+
+TEST(ConvexHull, mmm) {
+  std::random_device dev;
+  std::mt19937 gen(dev());
+  std::uniform_real_distribution<> urd(-1000, 1000);
+  std::vector<Coordinates> points;
+  Coordinates pt;
+  for (int i = 0; i < 6; i++) {
+    pt.set_x(urd(gen));
+    pt.set_y(urd(gen));
+    points.push_back(pt);
+  }
+  std::vector<Coordinates> hull = graham(points);
+}
+
+TEST(ConvexHull, eee) {
+  std::random_device dev;
+  std::mt19937 gen(dev());
+  std::uniform_real_distribution<> urd(-1000, 1000);
+  std::vector<Coordinates> points;
+  Coordinates pt;
+  for (int i = 0; i < 9; i++) {
+    pt.set_x(urd(gen));
+    pt.set_y(urd(gen));
+    points.push_back(pt);
+  }
+  std::vector<Coordinates> hull = graham(points);
+}
