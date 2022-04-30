673c6c165716e3c839cd0c3f065e3c8bf5a18b1d
OlgaTyulkina <yalo2001q@yandex.ru>
2022-04-30 23:09:49 +0300

    fix_commit

--- modules/task_1/tulkina_o_labeling/labeling.cpp
+++ modules/task_1/tulkina_o_labeling/labeling.cpp
@@ -4,45 +4,44 @@
 std::vector<std::vector<int>> labeling(
     const std::vector<std::vector<int>>& binary_image) {
   int width = binary_image[0].size(),
-      height = binary_image.size();  // ширина и высота изображения
+      height = binary_image.size();  
 
   Union un(height * width + 1);
   std::vector<std::vector<int>> res(
-      height, std::vector<int>(width));  // вектор результат
+      height, std::vector<int>(width)); 
 
-  for (int y = 0; y < height; y++) {  // проход по изображению
+  for (int y = 0; y < height; y++) {  
     for (int x = 0; x < width; x++) {
-      if (binary_image[y][x] > 0) {  // пропустить фоновую компоненту
+      if (binary_image[y][x] > 0) { 
+        int index = y * width + x + 1;  
 
-        int index = y * width + x + 1;  // локальное значение компоненты
-
-        if (x > 0 && binary_image[y][x - 1]) {  // проверка идентификатора
-                                                // соседней компоненты
+        if (x > 0 && binary_image[y][x - 1]) {  
+                                                
           un.set_label(index,
-                       y * width + x - 1 + 1);  // объединить две компоненты
+                       y * width + x - 1 + 1);  
         }
-        if (y > 0 && binary_image[y - 1][x]) {  // проверка идентификатора
-                                                // соседней компоненты
+        if (y > 0 && binary_image[y - 1][x]) {
+                                                
           un.set_label(index,
-                       (y - 1) * width + x + 1);  // объединить две компоненты
+                       (y - 1) * width + x + 1);  
         }
         if ((y > 0 && x > 0) &&
-            binary_image[y - 1][x - 1]) {  // проверка идентификатора соседней
-                                           // компоненты
+            binary_image[y - 1][x - 1]) {  
+                                           
           un.set_label(index, (y - 1) * width + (x - 1) +
-                                  1);  // объединить две компоненты
+                                  1); 
         }
         if ((y > 0 && x < width - 1) &&
-            binary_image[y - 1][x + 1]) {  // проверка идентификатора соседней
-                                           // компоненты
+            binary_image[y - 1][x + 1]) {  
+                                           
           un.set_label(index, (y - 1) * width + (x + 1) +
-                                  1);  // объединить две компоненты
+                                  1);  
         }
       }
     }
   }
 
-  for (int y = 0; y < height; y++) {  // маркировка
+  for (int y = 0; y < height; y++) {  
     for (int x = 0; x < width; x++) {
       if (binary_image[y][x] > 0) res[y][x] = un.get_label(y * width + x + 1);
     }
