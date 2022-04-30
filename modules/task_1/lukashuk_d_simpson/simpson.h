3cd96a55ecc08c5c6813396071ba2cff6f9c6e37
DianaLed <61021228+DianaLed@users.noreply.github.com>
2022-04-30 14:26:06 +0300

    ferst commit

---
+++ modules/task_1/lukashuk_d_simpson/simpson.h
@@ -0,0 +1,16 @@
+// Copyright 2022 Lukashuk Diana
+#ifndef MODULES_TASK_1_LUKASHUK_D_SIMPSON_SIMPSON_H_
+#define MODULES_TASK_1_LUKASHUK_D_SIMPSON_SIMPSON_H_
+
+#include <algorithm>
+#include <cmath>
+#include <utility>
+#include <vector>
+
+typedef double (*FUNC_T)(const std::vector<double>&);
+typedef std::vector<std::pair<double, double>> BORDER_T;
+
+double simpson(const FUNC_T& f, const BORDER_T& borders,
+               const std::vector<int>& steps);
+
+#endif  // MODULES_TASK_1_LUKASHUK_D_SIMPSON_SIMPSON_H_
