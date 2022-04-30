af4db97a6f960c23f72f4006a7b8b0aa0e906ee5
Nikolai Groshev <kolia.avatar@gmail.com>
2022-04-30 14:48:17 +0300

    style fix

--- modules/task_1/groshev_n_monte_carlo_integrals/integration_monte_carlo.h
+++ modules/task_1/groshev_n_monte_carlo_integrals/integration_monte_carlo.h
@@ -1,6 +1,6 @@
 // Copyright 2022 Groshev Nickolay
-#ifndef MODULES_TASK_1_GROSHEV_N_INTEGRATION_MONTE_CARLO_INTEGRATION_MONTE_CARLO_H_
-#define MODULES_TASK_1_GROSHEV_N_INTEGRATION_MONTE_CARLO_INTEGRATION_MONTE_CARLO_H_
+#ifndef MODULES_TASK_1_GROSHEV_N_MONTE_CARLO_INTEGRALS_INTEGRATION_MONTE_CARLO_H_
+#define MODULES_TASK_1_GROSHEV_N_MONTE_CARLO_INTEGRALS_INTEGRATION_MONTE_CARLO_H_
 
 #include <vector>
 #include <functional>
@@ -8,4 +8,4 @@
 double getIntegralSequential(int n, const std::vector<double>& low, const std::vector<double>& top,
   const std::function<double(const std::vector<double>&)>& f);
 
-#endif  // MODULES_TASK_1_GROSHEV_N_INTEGRATION_MONTE_CARLO_INTEGRATION_MONTE_CARLO_H_
+#endif  // MODULES_TASK_1_GROSHEV_N_MONTE_CARLO_INTEGRALS_INTEGRATION_MONTE_CARLO_H_
--- modules/task_1/groshev_n_monte_carlo_integrals/main.cpp
+++ modules/task_1/groshev_n_monte_carlo_integrals/main.cpp
@@ -1,9 +1,9 @@
 // Copyright 2022 Groshev Nickolay
+#include <time.h>
 #include <gtest/gtest.h>
 #include <vector>
 #include <cmath>
 #include "./integration_monte_carlo.h"
-#include <time.h>
 
 std::function<double(const std::vector<double>&)> f1 = [](const std::vector<double>& x) {
   return 2 * x[0];
@@ -32,29 +32,41 @@ TEST(Integration_Monte_Carlo_Seq, dimension_is_1) {
   clock_t start = clock();
   double res = getIntegralSequential(200000, { low }, { top }, f1);
   clock_t end = clock();
-  double seconds = (double)(end - start) / CLOCKS_PER_SEC;
-  printf("The time: %f seconds\n", seconds);
+  double seconds = static_cast<double>(end - start) / CLOCKS_PER_SEC;
+  printf("Sequence algorithm: %f seconds\n", seconds);
   ASSERT_NEAR(res, 9.0, 0.07);
 }
 
 TEST(Integration_Monte_Carlo_Seq, dimension_is_2) {
   std::vector<double>low = { -1.0, 1.0 };
   std::vector<double>top = { 0.0, 2.0 };
+  clock_t start = clock();
   double res = getIntegralSequential(200000, low, top, f2);
+  clock_t end = clock();
+  double seconds = static_cast<double>(end - start) / CLOCKS_PER_SEC;
+  printf("Sequence algorithm: %f seconds\n", seconds);
   ASSERT_NEAR(res, 1.5, 0.07);
 }
 
 TEST(Integration_Monte_Carlo_Seq, mdimension_is_3_v1) {
   std::vector<double>low = { -4.0, 0.0, 1.5 };
   std::vector<double>top = { -3.0, 5.0, 3.0 };
+  clock_t start = clock();
   double res = getIntegralSequential(200000, low, top, f3);
+  clock_t end = clock();
+  double seconds = static_cast<double>(end - start) / CLOCKS_PER_SEC;
+  printf("Sequence algorithm: %f seconds\n", seconds);
   ASSERT_NEAR(res, 9.375, 0.07);
 }
 
 TEST(Integration_Monte_Carlo_Seq, dimension_is_3_v2) {
   std::vector<double>low = { 0.15, 0.0, 1.5 };
   std::vector<double>top = { 1.0, 2.0, 3.0 };
+  clock_t start = clock();
   double res = getIntegralSequential(200000, low, top, f4);
+  clock_t end = clock();
+  double seconds = static_cast<double>(end - start) / CLOCKS_PER_SEC;
+  printf("Sequence algorithm: %f seconds\n", seconds);
   ASSERT_NEAR(res, 6.62486548705585, 0.07);
 }
 
