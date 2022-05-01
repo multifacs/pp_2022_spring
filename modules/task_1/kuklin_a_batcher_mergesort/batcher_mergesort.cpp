1f22f0a3887c192c0f0ea6aa304acdb9e841fd2e
Andrey Kuklin <andre.kuk@bk.ru>
2022-05-01 21:25:47 +0300

    Add batcher merge

--- modules/task_1/kuklin_a_batcher_mergesort/batcher_mergesort.cpp
+++ modules/task_1/kuklin_a_batcher_mergesort/batcher_mergesort.cpp
@@ -5,23 +5,23 @@
 #include <random>
 
 using vector_d = std::vector<double>;
+constexpr size_t bv = 256;
 
-vector_d getRandVec(size_t sz, double lower_bound, double upper_bound) {
-  vector_d vec(sz);
+vector_d getRandVec(size_t vec_size, double lower_bound, double upper_bound) {
+  vector_d vec(vec_size);
   std::uniform_real_distribution<double> distribution(lower_bound, upper_bound);
   std::random_device device;
   std::mt19937 rnd(device());
 
-  for (size_t i = 0; i < sz; ++i) vec[i] = distribution(rnd);
+  for (size_t i = 0; i < vec_size; ++i) vec[i] = distribution(rnd);
 
   return vec;
 }
 
 std::vector<vector_d> genDigitCounters(vector_d* source_vec, size_t elem_num) {
   std::vector<vector_d> digitCounters(256);
-  int j = 0;
+  int index_sv = 0;
   int curr_byte = 0;
-  size_t local_size = 0;
 
   for (size_t byte_num = 0; byte_num < sizeof(double); ++byte_num) {
     for (size_t elem_ind = 0; elem_ind < elem_num; ++elem_ind) {
@@ -30,44 +30,41 @@ std::vector<vector_d> genDigitCounters(vector_d* source_vec, size_t elem_num) {
       digitCounters[curr_byte].push_back((*source_vec)[elem_ind]);
     }
 
-    for (size_t i = 0; i < 256; ++i) {
+  size_t count_size = 0;
+    for (size_t i = 0; i < bv; ++i) {
       if (!digitCounters.empty()) {
-        local_size = digitCounters[i].size();
-        for (size_t k = 0; k < local_size; ++k) {
-          (*source_vec)[j] = digitCounters[i][k];
-          ++j;
+        count_size = digitCounters[i].size();
+        for (size_t j = 0; j < count_size; ++j) {
+          (*source_vec)[index_sv++] = digitCounters[i][j];
         }
         digitCounters[i].clear();
       }
     }
-    j = 0;
+    index_sv = 0;
   }
 
   return digitCounters;
 }
 
 void radixPass(vector_d* source_vec, const std::vector<vector_d>& digitCounters) {
-  size_t size;
-  size_t j = 0;
+  size_t index_v = 0;
+  size_t count_size = 0;
 
-  for (size_t i = 255; i >= 128; --i) {
+  for (size_t i = bv - 1; i >= bv / 2; --i) {
     if (!digitCounters[i].empty()) {
-      size = digitCounters[i].size();
-      for (size_t k = size - 1; k >= 1; --k) {
-        (*source_vec)[j] = digitCounters[i][k];
-        j++;
+      count_size = digitCounters[i].size();
+      for (size_t k = count_size - 1; k >= 1; --k) {
+        (*source_vec)[index_v++] = digitCounters[i][k];
       }
-      (*source_vec)[j] = digitCounters[i][0];
-      j++;
+      (*source_vec)[index_v++] = digitCounters[i][0];
     }
   }
 
-  for (size_t i = 0; i < 128; ++i) {
+  for (size_t i = 0; i < bv / 2; ++i) {
     if (!digitCounters[i].empty()) {
-      size = digitCounters[i].size();
-      for (size_t k = 0; k < size; ++k) {
-        (*source_vec)[j] = digitCounters[i][k];
-        j++;
+      count_size = digitCounters[i].size();
+      for (size_t j = 0; j < count_size; ++j) {
+        (*source_vec)[index_v++] = digitCounters[i][j];
       }
     }
   }
@@ -78,11 +75,96 @@ void floatRadixSort(vector_d* source_vec) {
 
   auto digitCounters = genDigitCounters(source_vec, elem_num);
 
-  int tmp = 0;
-  for (size_t i = 0; i < elem_num; i++) {
-    tmp = static_cast<int>(*((unsigned char*)&(*source_vec)[i] + 7));
-    digitCounters[tmp].push_back((*source_vec)[i]);
+  int count_index = 0;
+  for (size_t i = 0; i < elem_num; ++i) {
+    count_index = static_cast<int>(*((unsigned char*)&(*source_vec)[i] + 7));
+    digitCounters[count_index].push_back((*source_vec)[i]);
   }
 
   radixPass(source_vec, digitCounters);
+}
+
+void evenSplitter(vector_d& res_vec, const vector_d& first_vec,
+                  const vector_d& second_vec) {
+  size_t index_a = 0;
+  size_t index_b = 0;
+  size_t i = 0;
+
+  auto first_size = first_vec.size();
+  auto second_size = second_vec.size();
+
+  while ((index_a < first_size) && (index_b < second_size)) {
+    if (first_vec[index_a] <= second_vec[index_b]) {
+      res_vec[i] = first_vec[index_a];
+      index_a += 2;
+    } else {
+      res_vec[i] = second_vec[index_b];
+      index_b += 2;
+    }
+    i += 2;
+  }
+
+  if (index_a >= first_size)
+    for (size_t j = index_b; j < second_size && i < first_size + second_size;
+         j += 2, i += 2)
+      res_vec[i] = second_vec[j];
+  else
+    for (size_t j = index_a; j < first_size && i < first_size + second_size;
+         j += 2, i += 2)
+      res_vec[i] = first_vec[j];
+}
+
+void oddSplitter(vector_d& res_vec, const vector_d& first_vec,
+                 const vector_d& second_vec) {
+  size_t index_a = 1;
+  size_t index_b = 1;
+  size_t i = 1;
+
+  auto first_size = first_vec.size();
+  auto second_size = second_vec.size();
+
+  while ((index_a < first_size) && (index_b < second_size)) {
+    if (first_vec[index_a] <= second_vec[index_b]) {
+      res_vec[i] = first_vec[index_a];
+      index_a += 2;
+    } else {
+      res_vec[i] = second_vec[index_b];
+      index_b += 2;
+    }
+    i += 2;
+  }
+
+  if (index_a >= first_size)
+    for (size_t j = index_b; j < second_size && i < first_size + second_size;
+         j += 2, i += 2)
+      res_vec[i] = second_vec[j];
+  else
+    for (size_t j = index_a; j < first_size && i < first_size + second_size;
+         j += 2, i += 2)
+      res_vec[i] = first_vec[j];
+
+  if (first_size % 2 == 1 && second_size % 2 == 1) {
+    res_vec[first_size + second_size - 1] =
+        std::max(first_vec[first_size - 1], second_vec[second_size - 1]);
+  }
+}
+
+void batcherComparator(vector_d& res_vec) {
+  auto res_size = res_vec.size();
+  for (size_t i = 1; i < res_size; ++i) {
+    if (res_vec[i] < res_vec[i - 1])
+      std::swap(res_vec[i], res_vec[i - 1]);
+  }
+}
+
+vector_d batcherMerge(vector_d& first_vec, vector_d& second_vec) {
+  auto first_size = first_vec.size();
+  auto second_size = second_vec.size();
+  vector_d res_vec(first_size + second_size);
+
+  evenSplitter(res_vec, first_vec, second_vec);
+  oddSplitter(res_vec, first_vec, second_vec);
+  batcherComparator(res_vec);
+
+  return res_vec;
 }
--- modules/task_1/kuklin_a_batcher_mergesort/batcher_mergesort.h
+++ modules/task_1/kuklin_a_batcher_mergesort/batcher_mergesort.h
@@ -6,10 +6,16 @@
 
 using vector_d = std::vector<double>;
 
-std::vector<double> getRandVec(size_t sz, double lower_bound,
+std::vector<double> getRandVec(size_t vec_size, double lower_bound,
                                double upper_bound);
 std::vector<vector_d> genDigitCounters(vector_d* source_vec, size_t elem_num);
 void radixPass(vector_d* source_vec, const std::vector<vector_d>& digitCounters);
 void floatRadixSort(vector_d* source_vec);
+void evenSplitter(vector_d& res_vec, const vector_d& first_vec,
+                  const vector_d& second_vec);
+void oddSplitter(vector_d& res_vec, const vector_d& first_vec,
+                 const vector_d& second_vec);
+void batcherComparator(vector_d& res_vec);
+vector_d batcherMerge(vector_d& first_vec, vector_d& second_vec);
 
 #endif  // MODULES_TASK_1_KUKLIN_A_BATCHER_MERGESORT_BATCHER_MERGESORT_H_
