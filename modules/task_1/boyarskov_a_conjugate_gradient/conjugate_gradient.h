85b83f02c300bbde7f0c45defb2aba261f77ec3a
ABoyarskov <anboyarskov@yandex.ru>
2022-05-02 23:06:51 +0300

    first commit

---
+++ modules/task_1/boyarskov_a_conjugate_gradient/conjugate_gradient.h
@@ -0,0 +1,28 @@
+// Copyright 2022 Boyarskov Anton
+#ifndef MODULES_TASK_1_BOYARSKOV_A_CONJUGATE_GRADIENT_CONJUGATE_GRADIENT_H_
+#define MODULES_TASK_1_BOYARSKOV_A_CONJUGATE_GRADIENT_CONJUGATE_GRADIENT_H_
+
+#include <algorithm>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+
+const double NEARZERO = 1.0e-10;  // interpretation of "zero"
+
+using vec = std::vector<double>;  // vector
+using matrix = std::vector<vec>;  // matrix (=collection of (row) vectors)
+
+// Prototypes
+vec generateRandomVector(int size);
+matrix generateMatrix(vec v);
+void print(const std::string& title, const vec& V);
+void print(const std::string& title, const matrix& A);
+void printError(const vec& B, const vec& check, int n);
+double scalarProduct(const vec& V, const vec& U);
+vec matrixTimesVector(const matrix& A, const vec& V);
+vec conjugateGradientSolver(const matrix& A, const vec& V);
+
+#endif  // MODULES_TASK_1_BOYARSKOV_A_CONJUGATE_GRADIENT_CONJUGATE_GRADIENT_H_
