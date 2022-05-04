91dece01573822142d7034d458f79c0040142be7
CheremshkinKI <kenodi.d@mail.ru>
2022-05-04 16:24:35 +0300

    fffffffffff

--- modules/task_2/cheremushkin_a_matrix_multiplication_omp/ComplexMatrix.cpp
+++ modules/task_2/cheremushkin_a_matrix_multiplication_omp/ComplexMatrix.cpp
@@ -201,9 +201,10 @@ MatrixComplex MatrixComplex::Multiply_parallel
     std::complex<int> tmp;
     std::vector<std::vector<std::complex<int>>>
         resultMatrix = getDummyM(left.Size);
-// #pragma omp parallel for shared(left, right, resultMatrix) private(tmp)
-        for (int i = 0; i < left.Size; i++) {
-            for (int j = 0; j < left.Size; j++) {
+    int i, j;
+#pragma omp parallel for shared(left, right, resultMatrix) private(tmp, i, j)
+        for (i = 0; i < left.Size; i++) {
+            for (j = 0; j < left.Size; j++) {
                 tmp = std::complex<int>();
                 for (int k = 0; k < left.Size; k++) {
                     tmp += left.get(k, i) * right.get(k, j);
