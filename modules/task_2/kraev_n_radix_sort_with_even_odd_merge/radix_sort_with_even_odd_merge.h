4ebb5a7f0c14c12775ac38141926e0092da90f3c
NickitaKraev <75929318+NickitaKraev@users.noreply.github.com>
2022-05-05 22:31:09 +0300

    fixed codestyle

--- modules/task_2/kraev_n_radix_sort_with_even_odd_merge/radix_sort_with_even_odd_merge.h
+++ modules/task_2/kraev_n_radix_sort_with_even_odd_merge/radix_sort_with_even_odd_merge.h
@@ -7,7 +7,8 @@
 
 void radixSort(std::vector<double>* vec, size_t left, size_t right);
 std::vector<double> getRandomVector(int size);
-void evenOrOddSplitter(std::vector<double>* vec, size_t left_1, size_t right_1, size_t left_2, size_t right_2, bool check);
+void evenOrOddSplitter(std::vector<double>* vec, size_t left_1, size_t right_1,
+                       size_t left_2, size_t right_2, bool check);
 void simpleComparator(std::vector<double>* vec, size_t left, size_t right);
 int findNumberPhaseMerge(int size);
 void parallelRadixSort(std::vector<double>* vec, int number_threads, int size);
