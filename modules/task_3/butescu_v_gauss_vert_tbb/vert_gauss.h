c845d394b45321d7fc00f3fb4c9c4ff046989a4e
Владимир Бутеску <vbutescu2000@gmail.com>
2022-05-05 15:19:43 +0300

    V1

---
+++ modules/task_3/butescu_v_gauss_vert_tbb/vert_gauss.h
@@ -0,0 +1,19 @@
+//  Copyright 2022 Butescu Vladimir
+#ifndef MODULES_TASK_3_BUTESCU_V_GAUSS_VERT_TBB_VERT_GAUSS_H_
+#define MODULES_TASK_3_BUTESCU_V_GAUSS_VERT_TBB_VERT_GAUSS_H_
+
+#include <vector>
+#include <random>
+#include <ctime>
+#include <tbb/tick_count.h>
+#include <tbb/parallel_for.h>
+
+
+int clamp(int value, int max, int min);
+std::vector<double> createRandomMatrix(int rows, int cols);
+std::vector<double> gauss_filter_sequence(const std::vector<double>& matrix,
+    int rows, int cols);
+std::vector<double> gauss_filter_parralel_tbb(const std::vector<double>& matrix,
+    int rows, int cols);
+
+#endif  // MODULES_TASK_3_BUTESCU_V_GAUSS_VERT_TBB_VERT_GAUSS_H_
