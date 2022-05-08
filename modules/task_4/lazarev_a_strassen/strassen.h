a71ea59c0bc154989ab7d3b9aeb44dc40f0c6a78
Nick Shell <kiwi0tails@gmail.com>
2022-05-08 19:36:46 +0300

    strassen std

---
+++ modules/task_4/lazarev_a_strassen/strassen.h
@@ -0,0 +1,19 @@
+// Copyright 2022 Lazarev Aleksey
+#ifndef MODULES_TASK_4_LAZAREV_A_STRASSEN_STRASSEN_H_
+#define MODULES_TASK_4_LAZAREV_A_STRASSEN_STRASSEN_H_
+
+#include <string>
+#include <thread>  // NOLINT
+#include <vector>
+
+const int threshold = 256;
+
+using vector = std::vector<int>;
+using matrix = std::vector<std::vector<int>>;
+
+void setToRandom(matrix* A, int n);
+
+matrix strassenMultiply(matrix* A, matrix* B, int n, bool parallel = true);
+matrix multiply(matrix* A, matrix* B, int n);
+
+#endif  // MODULES_TASK_4_LAZAREV_A_STRASSEN_STRASSEN_H_
