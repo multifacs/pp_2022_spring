e742482c05abde62d4935b79a4c5ddbc5ca357e1
Nick Shell <kiwi0tails@gmail.com>
2022-05-08 18:29:32 +0300

    simpson tbb fix

--- modules/task_3/votyakova_d_simpson/simpson.h
+++ modules/task_3/votyakova_d_simpson/simpson.h
@@ -2,20 +2,19 @@
 #ifndef MODULES_TASK_3_VOTYAKOVA_D_SIMPSON_SIMPSON_H_
 #define MODULES_TASK_3_VOTYAKOVA_D_SIMPSON_SIMPSON_H_
 
+#include <tbb/tbb.h>
+
 #include <algorithm>
 #include <functional>
 #include <utility>
 #include <vector>
 
-#include <tbb/tbb.h>
-
 double getSeqSimpson(const std::function<double(std::vector<double>)>& f,
                      const std::vector<std::pair<double, double>>& limits,
                      const std::vector<int>& n);
 
-double getTBBSimpson(
-    const std::function<double(std::vector<double>)>& f,
-    const std::vector<std::pair<double, double>>& limits,
-    const std::vector<int>& n);
+double getTBBSimpson(const std::function<double(std::vector<double>)>& f,
+                     const std::vector<std::pair<double, double>>& limits,
+                     const std::vector<int>& n);
 
 #endif  // MODULES_TASK_3_VOTYAKOVA_D_SIMPSON_SIMPSON_H_
