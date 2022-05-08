717165edf9eb070b1b20ed7b24a70099171dcf79
Nick Shell <kiwi0tails@gmail.com>
2022-05-08 17:35:35 +0300

    strassen tbb

---
+++ modules/task_3/lazarev_a_strassen/strassen.h
@@ -0,0 +1,23 @@
+// Copyright 2022 Lazarev Aleksey
+#ifndef MODULES_TASK_3_LAZAREV_A_STRASSEN_STRASSEN_H_
+#define MODULES_TASK_3_LAZAREV_A_STRASSEN_STRASSEN_H_
+
+#include <tbb/blocked_range.h>
+#include <tbb/parallel_reduce.h>
+#include <tbb/task_scheduler_init.h>
+#include <tbb/tbb.h>
+
+#include <string>
+#include <vector>
+
+const int threshold = 256;
+
+using vector = std::vector<int>;
+using matrix = std::vector<std::vector<int>>;
+
+void setToRandom(matrix* A, int n);
+
+matrix strassenMultiply(matrix* A, matrix* B, int n, bool parallel = true);
+matrix multiply(matrix* A, matrix* B, int n);
+
+#endif  // MODULES_TASK_3_LAZAREV_A_STRASSEN_STRASSEN_H_
