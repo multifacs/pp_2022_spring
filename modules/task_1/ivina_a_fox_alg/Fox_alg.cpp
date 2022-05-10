9e0bc83ba16db3eec98a6611f8a7d63c5cd76632
Anastasya Ivina <adelinesommers@gmail.com>
2022-05-10 21:09:05 +0300

    fix

--- modules/task_1/ivina_a_fox_alg/Fox_alg.cpp
+++ modules/task_1/ivina_a_fox_alg/Fox_alg.cpp
@@ -6,98 +6,75 @@
 #include <iostream>
 #include <iomanip>
 
-Matrix FillMatrixRandom(Matrix &matrix)
-{
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<> dis(0.0, 1000.0);
-    if (matrix.size() > 0)
-    {
-        for (int i = 0; i < matrix.size(); i++)
-        {
-            for (int j = 0; j < matrix[0].size(); j++)
-            {
-                matrix[i][j] = dis(gen);
-            }
-        }
+Matrix FillMatrixRandom(const Matrix &m) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> dis(0.0, 1000.0);
+  Matrix matrix {m};
+  if (matrix.size() > 0) {
+    for (size_t i = 0; i < matrix.size(); i++) {
+      for (size_t j = 0; j < matrix[0].size(); j++) {
+        matrix[i][j] = dis(gen);
+      }
     }
-    else
-    {
-        throw "Invalid matrix size";
-    }
+  } else {
+    throw "Invalid matrix size";
+  }
 
-    return matrix;
+  return matrix;
 }
 
-void PrintMatrix(const Matrix &matrix)
-{
-    const int row = matrix.size();
-    const int col = matrix[0].size();
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < col; j++)
-        {
-            std::cout << matrix[i][j] << std::setw(4) << "  ";
-        }
-        std::cout << std::endl;
+void PrintMatrix(const Matrix &matrix) {
+  const size_t row = matrix.size();
+  const size_t col = matrix[0].size();
+  for (size_t i = 0; i < row; i++) {
+    for (size_t j = 0; j < col; j++) {
+      std::cout << matrix[i][j] << std::setw(4) << "  ";
     }
+    std::cout << std::endl;
+  }
 }
 
-Matrix DenseMatrixMultiplication(const Matrix &a,
-                                 const Matrix &b)
-{
+Matrix DenseMatrixMultiplication(const Matrix &a, const Matrix &b) {
+  if (a[0].size() != b.size()) {
+    throw "Matrices with different sizes cannot be multiplied";
+  }
+  const size_t row = a.size();
+  const size_t col = b[0].size();
+  Matrix c(row, MatrixRow(col, 0));
 
-    if (a[0].size() != b.size())
-    {
-        throw "Matrices with different sizes cannot be multiplied";
+  for (size_t i = 0; i < row; i++) {
+    for (size_t j = 0; j < col; j++) {
+      for (size_t k = 0; k < a[0].size(); k++) {
+        c[i][j] += a[i][k] * b[k][j];
+      }
     }
-    const int row = a.size();
-    const int col = b[0].size();
-    Matrix c(row, MatrixRow(col, 0));
-
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < col; j++)
-        {
-            for (int k = 0; k < a[0].size(); k++)
-            {
-                c[i][j] += a[i][k] * b[k][j];
-            }
-        }
-    }
-    return c;
+  }
+  return c;
 }
 
-Matrix BlockMatrixMultiplication(const Matrix &a,
-                                 const Matrix &b)
-{
-    if (a.size() != b.size())
-    {
-        throw "Matrices with different sizes cannot be multiplied";
-    }
-    const int row = a.size();
-    const int col = b[0].size();
-    Matrix c(row, MatrixRow(col, 0));
-
-    const int block_size = std::sqrt(row);
-
-    for (int ii = 0; ii < row; ii += block_size)
-    {
-        for (int jj = 0; jj < row; jj += block_size)
-        {
-            for (int i = 0; i < row; i++)
-            {
-                for (int j = ii; j < ((ii + block_size) > row ? row : (ii + block_size)); j++)
-                {
-                    double temp = 0;
-                    for (int k = jj; k < ((jj + block_size) > row ? row : (jj + block_size)); k++)
-                    {
-                        temp += a[i][k] * b[k][j];
-                    }
-                    c[i][j] += temp;
-                }
-            }
+Matrix BlockMatrixMultiplication(const Matrix &a, const Matrix &b) {
+  if (a.size() != b.size()) {
+    throw "Matrices with different sizes cannot be multiplied";
+  }
+  const size_t row = a.size();
+  const size_t col = b[0].size();
+  Matrix c(row, MatrixRow(col, 0));
+  const size_t block_size = std::sqrt(row);
+  for (size_t ii = 0; ii < row; ii += block_size) {
+    for (size_t jj = 0; jj < row; jj += block_size) {
+      for (size_t i = 0; i < row; i++) {
+        for (size_t j = ii;
+             j < ((ii + block_size) > row ? row : (ii + block_size)); j++) {
+          double temp = 0;
+          for (size_t k = jj;
+               k < ((jj + block_size) > row ? row : (jj + block_size)); k++) {
+            temp += a[i][k] * b[k][j];
+          }
+          c[i][j] += temp;
         }
+      }
     }
-    return c;
+  }
+  return c;
 }
--- modules/task_1/ivina_a_fox_alg/Fox_alg.h
+++ modules/task_1/ivina_a_fox_alg/Fox_alg.h
@@ -1,17 +1,16 @@
 // Copyright 2022 Ivina Anastasiya
-#ifndef MODULES_TASK_1_IVINA_A_ALG_FOX_ALG_FOX_H_
-#define MODULES_TASK_1_IVINA_A_ALG_FOX_ALG_FOX_H_
+#ifndef MODULES_TASK_1_IVINA_A_FOX_ALG_FOX_ALG_H_
+#define MODULES_TASK_1_IVINA_A_FOX_ALG_FOX_ALG_H_
 
 #include <vector>
 
 using Matrix = std::vector<std::vector<double>>;
 using MatrixRow = std::vector<double>;
 
-Matrix FillMatrixRandom(Matrix &matrix);
+Matrix FillMatrixRandom(const Matrix &matrix);
 void PrintMatrix(const Matrix &Matrix);
-Matrix DenseMatrixMultiplication(const Matrix &a,
-                                 const Matrix &b);
-Matrix BlockMatrixMultiplication(const Matrix &a,
-                                 const Matrix &b);
+Matrix DenseMatrixMultiplication(const Matrix &a, const Matrix &b);
+Matrix BlockMatrixMultiplication(const Matrix &a, const Matrix &b);
 
-#endif // MODULES_TASK_1_IVINA_A_ALG_FOX_ALG_FOX_H_
+#endif  // MODULES_TASK_1_IVINA_A_FOX_ALG_FOX_ALG_H_
+
