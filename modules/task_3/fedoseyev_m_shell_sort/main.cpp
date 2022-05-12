dbb59d864a18ef16f097276547a8d4fb3d32f137
Nick Shell <kiwi0tails@gmail.com>
2022-05-12 13:14:14 +0300

    shell tbb

---
+++ modules/task_3/fedoseyev_m_shell_sort/main.cpp
@@ -0,0 +1,63 @@
+// Copyright 2022 Fedoseyev Mikhail
+#include <gtest/gtest.h>
+#include <omp.h>
+
+#include "./shell_sort.h"
+
+TEST(SHELL_SORT_TBB, TEST_1) {
+  int size = 10;
+  int* a = generate_vector(size);
+  print_list(a, size);
+  shell_sort(a, size);
+  print_list(a, size);
+  ASSERT_EQ(check_list(a, size), 0);
+  delete[] a;
+}
+
+TEST(SHELL_SORT_TBB, TEST_2) {
+  int size = 20;
+  int* a = generate_vector(size);
+  shell_sort(a, size);
+  ASSERT_EQ(check_list(a, size), 0);
+  delete[] a;
+}
+
+TEST(SHELL_SORT_TBB, TEST_3) {
+  int size = 30;
+  int* a = generate_vector(size);
+  shell_sort(a, size);
+  ASSERT_EQ(check_list(a, size), 0);
+  delete[] a;
+}
+
+TEST(SHELL_SORT_TBB, TEST_4) {
+  int size = 30;
+  int* a = generate_vector(size);
+  shell_sort(a, size);
+  ASSERT_EQ(check_list(a, size), 0);
+  delete[] a;
+}
+
+const int big_size = 10;
+
+TEST(SHELL_SORT_STD, SIZE_BIG) {
+  int* a = generate_vector(big_size);
+  int* b = new int[big_size];
+  std::copy(a, a + big_size, b);
+
+  double start = omp_get_wtime();
+  shell_sort(a, big_size);
+  double end = omp_get_wtime();
+  double ptime = end - start;
+  std::cout << "\tparallel time: " << ptime << "\n";
+
+  shell_sort(b, big_size, false);
+  end = omp_get_wtime();
+  double stime = end - start;
+  std::cout << "\tsequential time: " << stime << "\n";
+
+  std::cout << "\tefficiency: " << stime / ptime << "\n";
+
+  delete[] a;
+  delete[] b;
+}
