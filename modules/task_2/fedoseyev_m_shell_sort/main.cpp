4c915833731391e9eaa34c15c286a55f00018955
MFredis <mishafedoseev701@gmail.com>
2022-05-12 13:40:05 +0300

    fix

--- modules/task_2/fedoseyev_m_shell_sort/main.cpp
+++ modules/task_2/fedoseyev_m_shell_sort/main.cpp
@@ -32,6 +32,15 @@ TEST(SHELL_SORT_OMP, TEST_3) {
   delete[] a;
 }
 
+TEST(SHELL_SORT_OMP, TEST_4) {
+	int size = 15;
+	int* a = generate_vector(size);
+	shell_sort(a, size);
+	shell_sort(a, size, false);
+	ASSERT_EQ(check_list(a, size), 0);
+	delete[] a;
+}
+
 const int big_size = 10;
 
 TEST(SHELL_SORT_OMP, SIZE_BIG) {
