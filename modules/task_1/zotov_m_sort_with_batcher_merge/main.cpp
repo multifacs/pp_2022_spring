939afd1518fc45d6768c30518f88a1638767e8d4
fonvizzzin <zotovmaksim2@mail.ru>
2022-05-12 12:00:15 +0300

    fix code style

--- modules/task_1/zotov_m_sort_with_batcher_merge/main.cpp
+++ modules/task_1/zotov_m_sort_with_batcher_merge/main.cpp
@@ -5,8 +5,8 @@
 #include "./sort_with_batcher_merge.h"
 
 TEST(zotov_radix_sort, Radix_Sort_1) {
-    std::vector<int> first_vector = {5,30,-10,-3,23,0,-1,16};
-    std::vector<int> second_vector = {-10,-3,-1,0,5,16,23,30};
+    std::vector<int> first_vector = { 5, 30, -10, -3, 23, 0, -1, 16 };
+    std::vector<int> second_vector = { -10, -3, -1, 0, 5, 16, 23, 30 };
     radixSort(&first_vector);
     ASSERT_EQ(second_vector, first_vector);
 }
@@ -20,7 +20,7 @@ TEST(zotov_radix_sort, Radix_Sort_2) {
 }
 
 TEST(zotov_radix_sort, Radix_Sort_3) {
-    std::vector<int> first_vector = getRandomVector(10000);
+    std::vector<int> first_vector = getRandomVector(1000);
     std::vector<int> second_vector = first_vector;
     radixSort(&first_vector);
     std::sort(second_vector.begin(), second_vector.end());
@@ -28,6 +28,14 @@ TEST(zotov_radix_sort, Radix_Sort_3) {
 }
 
 TEST(zotov_radix_sort, Radix_Sort_4) {
+    std::vector<int> first_vector = getRandomVector(10000);
+    std::vector<int> second_vector = first_vector;
+    radixSort(&first_vector);
+    std::sort(second_vector.begin(), second_vector.end());
+    ASSERT_EQ(second_vector, first_vector);
+}
+
+TEST(zotov_radix_sort, Radix_Sort_5) {
     std::vector<int> first_vector = getRandomVector(100000);
     std::vector<int> second_vector = first_vector;
     radixSort(&first_vector);
