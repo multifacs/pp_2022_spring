6a71ebee32e8f69ef5f5e3e5b21138f6ec05a1af
Nick Shell <kiwi0tails@gmail.com>
2022-05-17 03:46:39 +0300

    radix tbb

--- modules/task_3/sorokina_e_radix_int_simple_merge/main.cpp
+++ modules/task_3/sorokina_e_radix_int_simple_merge/main.cpp
@@ -20,7 +20,7 @@ void measureTime(INT_VEC* vec1, INT_VEC* vec2, int parts, bool parallel, bool pa
   std::cout << "speed: " << seq_time / par_time << "\n\n";
 }
 
-TEST(RADIX_INT_SIMPLE_MERGE_OMP, TEST_1) {
+TEST(RADIX_INT_SIMPLE_MERGE_TBB, TEST_1) {
   int n = 10;
   int parts = 3;
   INT_VEC vec = getRandomVec(n);
@@ -29,7 +29,7 @@ TEST(RADIX_INT_SIMPLE_MERGE_OMP, TEST_1) {
   ASSERT_TRUE(checkOrder(vec));
 }
 
-TEST(RADIX_INT_SIMPLE_MERGE_OMP, TEST_2) {
+TEST(RADIX_INT_SIMPLE_MERGE_TBB, TEST_2) {
   int n = 11;
   int parts = 11;
   INT_VEC vec = getRandomVec(n);
@@ -38,7 +38,7 @@ TEST(RADIX_INT_SIMPLE_MERGE_OMP, TEST_2) {
   ASSERT_TRUE(checkOrder(vec));
 }
 
-TEST(RADIX_INT_SIMPLE_MERGE_OMP, TEST_3) {
+TEST(RADIX_INT_SIMPLE_MERGE_TBB, TEST_3) {
   int n = 12;
   int parts = 3;
   INT_VEC vec = getRandomVec(n);
@@ -47,7 +47,7 @@ TEST(RADIX_INT_SIMPLE_MERGE_OMP, TEST_3) {
   ASSERT_TRUE(checkOrder(vec));
 }
 
-TEST(RADIX_INT_SIMPLE_MERGE_OMP, TEST_4) {
+TEST(RADIX_INT_SIMPLE_MERGE_TBB, TEST_4) {
   int n = 13;
   int parts = 3;
   INT_VEC vec = getRandomVec(n);
@@ -56,7 +56,7 @@ TEST(RADIX_INT_SIMPLE_MERGE_OMP, TEST_4) {
   ASSERT_TRUE(checkOrder(vec));
 }
 
-TEST(RADIX_INT_SIMPLE_MERGE_OMP, TEST_PARALLEL_SORT_SEQ_MERGE) {
+TEST(RADIX_INT_SIMPLE_MERGE_TBB, TEST_PARALLEL_SORT_SEQ_MERGE) {
   int runs = 4;
   int n = 1000;
   int parts = omp_get_max_threads();
@@ -72,7 +72,7 @@ TEST(RADIX_INT_SIMPLE_MERGE_OMP, TEST_PARALLEL_SORT_SEQ_MERGE) {
   }
 }
 
-TEST(RADIX_INT_SIMPLE_MERGE_OMP, TEST_PARALLEL_SORT_PARALLEL_MERGE) {
+TEST(RADIX_INT_SIMPLE_MERGE_TBB, TEST_PARALLEL_SORT_PARALLEL_MERGE) {
   int runs = 4;
   int n = 1000;
   int parts = omp_get_max_threads();
---
+++ modules/task_3/sorokina_e_radix_int_simple_merge/radix_int_simple_merge.h
@@ -0,0 +1,15 @@
+// Copyright 2022 Sorokina Ekaterina
+#ifndef MODULES_TASK_3_SOROKINA_E_RADIX_INT_SIMPLE_MERGE_RADIX_INT_SIMPLE_MERGE_H_
+#define MODULES_TASK_3_SOROKINA_E_RADIX_INT_SIMPLE_MERGE_RADIX_INT_SIMPLE_MERGE_H_
+
+#include <algorithm>
+#include <vector>
+
+using INT_VEC = std::vector<int>;
+
+INT_VEC getRandomVec(int n);
+void radixSortSimpleMerge(INT_VEC* vec, int parts, bool parallel = true, bool parallel_merge = false);
+void printVec(const INT_VEC& vec);
+bool checkOrder(const INT_VEC& vec);
+
+#endif  // MODULES_TASK_3_SOROKINA_E_RADIX_INT_SIMPLE_MERGE_RADIX_INT_SIMPLE_MERGE_H_
