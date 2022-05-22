0ba0afaf8056a289aa7a18dedb5fdeafd5eff47f
Nikolai Groshev <kolia.avatar@gmail.com>
2022-05-22 13:27:41 +0300

    ifndef fix

--- modules/task_3/groshev_n_monte_carlo_integrals_tbb/integration_monte_carlo.h
+++ modules/task_3/groshev_n_monte_carlo_integrals_tbb/integration_monte_carlo.h
@@ -1,6 +1,6 @@
 // Copyright 2022 Groshev Nickolay
-#ifndef MODULES_TASK_2_GROSHEV_N_MONTE_CARLO_INTEGRALS_OMP_INTEGRATION_MONTE_CARLO_H_
-#define MODULES_TASK_2_GROSHEV_N_MONTE_CARLO_INTEGRALS_OMP_INTEGRATION_MONTE_CARLO_H_
+#ifndef MODULES_TASK_3_GROSHEV_N_MONTE_CARLO_INTEGRALS_TBB_INTEGRATION_MONTE_CARLO_H_
+#define MODULES_TASK_3_GROSHEV_N_MONTE_CARLO_INTEGRALS_TBB_INTEGRATION_MONTE_CARLO_H_
 
 #include <omp.h>
 #include <tbb/tbb.h>
@@ -12,4 +12,4 @@ double getIntegralSequential(int n, const std::vector<double>& low, const std::v
 
 double getIntegralTBB(int n, const std::vector<double>& low, const std::vector<double>& top,
     const std::function<double(const std::vector<double>&)>& f);
-#endif  // MODULES_TASK_2_GROSHEV_N_MONTE_CARLO_INTEGRALS_OMP_INTEGRATION_MONTE_CARLO_H_
+#endif  // MODULES_TASK_3_GROSHEV_N_MONTE_CARLO_INTEGRALS_TBB_INTEGRATION_MONTE_CARLO_H_
