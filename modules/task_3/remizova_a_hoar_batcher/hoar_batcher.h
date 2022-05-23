37850f1dc5eda4500cdb7c2d23157d6cd798b8bc
Remizova Antonina <Rearo5g@yandex.ru>
2022-05-23 22:10:15 +0300

    New task3

---
+++ modules/task_3/remizova_a_hoar_batcher/hoar_batcher.h
@@ -0,0 +1,20 @@
+// Copyright 2022 Remizova Antonina
+#ifndef MODULES_TASK_3_REMIZOVA_A_HOAR_BATCHER_HOAR_BATCHER_H_
+#define MODULES_TASK_3_REMIZOVA_A_HOAR_BATCHER_HOAR_BATCHER_H_
+
+#include <tbb/tbb.h>
+#include <random>
+#include <vector>
+
+std::vector<double> getRandVector(int size);
+bool isSortCorrectly(const std::vector <double>& vec);
+void hoarSort(std::vector <double>* vec, int left, int right);
+std::vector<double> evenBatcherSort(const std::vector<double>& vec1, const std::vector<double>& vec2);
+std::vector<double> oddBatcherSort(const std::vector<double>& vec1, const std::vector<double>& vec2);
+std::vector<double> oddEvenBatcherSort(const std::vector<double>& vec1, const std::vector<double>& vec2);
+std::vector<double> mergeBatcherSeq(const std::vector<double>& vec, int middle);
+std::vector<double> mergeBatcherPar(const std::vector<double>& vec, int middle);
+std::vector<double> hoarBatcherSeq(const std::vector<double>& vec, int size);
+std::vector<double> hoarBatcherPar(const std::vector<double>& vec, int size);
+
+#endif  // MODULES_TASK_3_REMIZOVA_A_HOAR_BATCHER_HOAR_BATCHER_H_
