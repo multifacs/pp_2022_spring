48c839df5801bfd46f73c1f9d571d0e80bd4a0ab
ZILLeF13 <71269813+ZILLeF13@users.noreply.github.com>
2022-05-23 23:40:09 +0300

    first commit

---
+++ modules/task_3/kirillov_n_monte_carlo_tbb/main.cpp
@@ -0,0 +1,77 @@
+// Copyright 2022 Kirillov Nikita
+
+#include <gtest/gtest.h>
+#include <tbb/tbb.h>
+
+#include <cmath>
+#include <functional>
+#include <vector>
+
+#include "./monte_carlo_tbb.h"
+
+const size_t N = 10000;
+
+TEST(MonteCarloIntegration, tbb_one_dims_sin) {
+  const size_t dims = 1;
+  std::function<double(const std::vector<double>&)> integrand =
+      [](const std::vector<double>& points) { return sin(points[0]); };
+  const double pi = 3.141592;
+  std::vector<std::pair<double, double>> intervals(dims);
+  intervals[0] = {0.0, pi / 2};
+
+  double answer = ParallelMonteCarloIntegration(integrand, intervals, N);
+  double real_answer = 1.0;
+
+  ASSERT_NEAR(real_answer, answer, 0.5);
+}
+
+TEST(MonteCarloIntegration, tbb_one_dims_quadratic) {
+  const size_t dims = 1;
+  std::function<double(const std::vector<double>&)> integrand =
+      [](const std::vector<double>& points) { return points[0] * points[0]; };
+  std::vector<std::pair<double, double>> intervals(dims);
+  intervals[0] = {0.0, 5.0};
+
+  double answer = ParallelMonteCarloIntegration(integrand, intervals, N);
+  double real_answer = 41.6667;
+
+  ASSERT_NEAR(real_answer, answer, 2.0);
+}
+
+TEST(MonteCarloIntegration, tbb_two_dims_hyperboloid) {
+  const size_t dims = 2;
+  std::function<double(const std::vector<double>&)> integrand =
+      [](const std::vector<double>& points) {
+        return pow(points[0], 2) + pow(points[1], 2);
+      };
+  std::vector<std::pair<double, double>> intervals(dims);
+  intervals[0] = {-2.0, 2.0};
+  intervals[1] = {-2.0, 2.0};
+
+  double answer = ParallelMonteCarloIntegration(integrand, intervals, N);
+  double real_answer = 42.6667;
+
+  ASSERT_NEAR(real_answer, answer, 2.0);
+}
+
+TEST(MonteCarloIntegration, tbb_two_dims_inverse_hyperboloid) {
+  const size_t dims = 2;
+  std::function<double(const std::vector<double>&)> integrand =
+      [](const std::vector<double>& points) {
+        return -pow(points[0], 2) - pow(points[1], 2) + 3;
+      };
+  std::vector<std::pair<double, double>> intervals(dims);
+  intervals[0] = {-2.0, 2.0};
+  intervals[1] = {-2.0, 2.0};
+
+  double answer = ParallelMonteCarloIntegration(integrand, intervals, N);
+  double real_answer = 5.3334;
+
+  ASSERT_NEAR(real_answer, answer, 2.0);
+}
+
+int main(int argc, char** argv) {
+  tbb::task_scheduler_init init(tbb::task_scheduler_init::automatic);
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
