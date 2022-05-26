4bcb7365e56a0ca79f7967cbdf21684c505a3883
DianaLed <61021228+DianaLed@users.noreply.github.com>
2022-05-26 17:04:58 +0300

    style commit

--- modules/task_3/lukashuk_d_simpson/simpson.h
+++ modules/task_3/lukashuk_d_simpson/simpson.h
@@ -3,6 +3,7 @@
 #define MODULES_TASK_3_LUKASHUK_D_SIMPSON_SIMPSON_H_
 
 #include <tbb/tbb.h>
+
 #include <algorithm>
 #include <cmath>
 #include <utility>
@@ -12,9 +13,9 @@ typedef double (*FUNC_T)(const std::vector<double>&);
 typedef std::vector<std::pair<double, double>> BORDER_T;
 
 double simpson(const FUNC_T& f, const BORDER_T& borders,
-    const std::vector<int>& steps);
+               const std::vector<int>& steps);
 
 double simpson_par(const FUNC_T& f, const BORDER_T& borders,
-    const std::vector<int>& steps);
+                   const std::vector<int>& steps);
 
 #endif  // MODULES_TASK_3_LUKASHUK_D_SIMPSON_SIMPSON_H_
