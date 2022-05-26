09da0124113493b39970c256fa64ce3906e86ae4
feoandrew <33204049+feoandrew@users.noreply.github.com>
2022-05-26 10:30:39 +0300

    style fix 2

--- modules/task_3/feoktistov_a_gauss_block/main.cpp
+++ modules/task_3/feoktistov_a_gauss_block/main.cpp
@@ -80,8 +80,6 @@ TEST(GaussianFilterBlock, Test_Wrong_Thread_Count) {
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
-  
   return RUN_ALL_TESTS();
-
   return 0;
 }
